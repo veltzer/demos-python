b434bb5a6eb5efe65dce922ede0c772999c9dc16
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-26 13:42:30 +0200

    example of json storage

---
+++ examples/short/technology/json_store.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import json # for dump, load
+
+my_dict={
+	'mark': 'veltzer',
+	'shay': 'sarid',
+}
+
+filename='/tmp/json_store.json'
+
+with open(filename, 'w') as f:
+	json.dump(my_dict, f, indent=4)
+
+with open(filename) as f:
+	other_dict = json.load(f)
+print(other_dict)
