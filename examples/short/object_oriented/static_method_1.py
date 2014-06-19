e2821143be9c26d71cb26d81cdaa034e218ee169
Udi Oron <udioron@gmail.com>
2014-06-19 14:48:58 +0300

    Just fix whitespace

--- examples/short/object_oriented/static_method_1.py
+++ examples/short/object_oriented/static_method_1.py
@@ -4,35 +4,42 @@
 This is a * sort * of static method but is ugly since the
 function is really global and not in the class.
 
-	Mark Veltzer <mark@veltzer.net>
+    Mark Veltzer <mark@veltzer.net>
 """
+
+
 class Book:
-	num=0
-	def __init__(self,price):
-		self.__price=price
-		Book.num+=1
-	def printit(self):
-		print('price is',self.__price)
-	def setPrice(self,newprice):
-		self.__price=newprice
+    num = 0
+
+    def __init__(self, price):
+        self.__price = price
+        Book.num += 1
+
+    def printit(self):
+        print('price is', self.__price)
+
+    def setPrice(self, newprice):
+        self.__price = newprice
+
+
 def getNumBooks():
-	return Book.num
+    return Book.num
 
 # lets create some books...
-b1=Book(14)
-b2=Book(13)
+b1 = Book(14)
+b2 = Book(13)
 
 # lets access the static member and the static methods...
-print('Book.num (direct access) is ',Book.num)
-print('getNumBooks() is ',getNumBooks())
+print('Book.num (direct access) is ', Book.num)
+print('getNumBooks() is ', getNumBooks())
 try:
-	print(b1.getNumBooks())
+    print(b1.getNumBooks())
 except AttributeError as e:
-	print('no,cannot access the static method via the instance')
+    print('no,cannot access the static method via the instance')
 # access the static member through an instance...
 print(b1.num)
 print(b2.num)
-b3=Book(12)
+b3 = Book(12)
 print(b1.num)
 print(b2.num)
 print(b3.num)
--- examples/short/object_oriented/static_method_3.py
+++ examples/short/object_oriented/static_method_3.py
@@ -6,23 +6,31 @@ Wrap it using the 'staticmethod' function...
 You can then call the method using the instance AND using
 the class.
 
-	Mark Veltzer <mark@veltzer.net>
+    Mark Veltzer <mark@veltzer.net>
 """
-class Book:
-	num=0
-	def __init__(self,price):
-		self.__price=price
-		Book.num+=1
-	def printit(self):
-		print('price is',self.__price)
-	def setPrice(self,newprice):
-		self.__price=newprice
-	def getNumBooks():
-		return Book.num
-	getNumBooks=staticmethod(getNumBooks)
 
-b1=Book(14)
-b2=Book(13)
+
+class Book:
+    num = 0
+
+    def __init__(self, price):
+        self.__price = price
+        Book.num += 1
+
+    def printit(self):
+        print('price is', self.__price)
+
+    def setPrice(self, newprice):
+        self.__price = newprice
+
+    def getNumBooks():
+        return Book.num
+
+    getNumBooks = staticmethod(getNumBooks)
+
+
+b1 = Book(14)
+b2 = Book(13)
 
 print(Book.num)
 print(b1.getNumBooks())
