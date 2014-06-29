b3815aa05028a2ba044d02a616f9201fb67bab29
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-29 13:41:57 +0300

    fixed up one example

--- examples/short/filesystem/expander.py
+++ examples/short/filesystem/expander.py
@@ -2,10 +2,11 @@
 
 """
 This example shows how to handle '~' in path names
+There are two different ways: expanduser and expandvars
 """
 
 from __future__ import print_function
+import os # for expanduser, expandvars
 
-import os
 print(os.path.expanduser('~/.viminfo'))
 print(os.path.expandvars('USER is $USER'))
