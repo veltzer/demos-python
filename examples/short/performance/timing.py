107873af4b6577d410606b1f892011479ca95386
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-12 20:34:44 +0300

    minor changes

--- examples/short/performance/timing.py
+++ examples/short/performance/timing.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
-"""
+'''
 Example of timing python code
 
 	Mark Veltzer <mark@veltzer.net>
-"""
+'''
 
 import time # for time, sleep
 
 time_before=time.time()
 time.sleep(2)
 time_after=time.time()
-print("time taken: {0:.3f} seconds".format(time_after-time_before))
+print('time taken: {0:.3f} seconds'.format(time_after-time_before))
