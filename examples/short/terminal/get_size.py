0f3f38a0d4146c5437521586818660b35d79e3ec
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-18 13:08:18 +0300

    improved this example and found the winner

--- examples/short/terminal/get_size.py
+++ examples/short/terminal/get_size.py
@@ -3,6 +3,8 @@
 """
 A program that shows you how to get the size of the terminal.
 
+A clear winner in terms of performance is the ioctl(2) one.
+
 References:
 http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
 
@@ -13,6 +15,7 @@ import os # for popen, environ
 import fcntl # for ioctl
 import struct # for unpack, pack
 import termios # for TIOCGWINSZ
+import time # for time
 
 def terminal_size1():
 	h, w, hp, wp = struct.unpack('HHHH',
@@ -27,21 +30,52 @@ def terminal_size3():
 	x=int(os.popen('tput cols', 'r').read())
 	y=int(os.popen('tput lines', 'r').read())
 	return (x,y)
-# this function does not work since LINES and COLUMNS are not exported
 def terminal_size4():
-	x=int(os.environ['LINES'])
-	y=int(os.environ['COLUMNS'])
-	return (x,y)
-def terminal_size5():
 	d=dict()
 	for tc_entry in os.environ['TERMCAP'].split(':'):
 		if tc_entry.find('#')!=-1:
 			key, val=tc_entry.split('#')
 			d[key]=val
 	return (int(d['co']), int(d['li']))
+# this function does not work since LINES and COLUMNS are not exported
+def terminal_size5():
+	x=int(os.environ['LINES'])
+	y=int(os.environ['COLUMNS'])
+	return (x,y)
 
 print(terminal_size1())
 print(terminal_size2())
 print(terminal_size3())
-#print(terminal_size4())
-print(terminal_size5())
+print(terminal_size4())
+#print(terminal_size5())
+
+# lets compare performance
+count=1000
+def do1():
+	for x in range(count):
+		terminal_size1()
+def do2():
+	for x in range(count):
+		terminal_size2()
+def do3():
+	for x in range(count):
+		terminal_size3()
+def do4():
+	for x in range(count):
+		terminal_size4()
+def do5():
+	for x in range(count):
+		terminal_size5()
+def time_it(f, desc):
+	time_before=time.time()
+	f()
+	time_after=time.time()
+	print('time taken for {desc}: {diff:.3f} seconds'.format(
+		diff=time_after-time_before,
+		desc=desc,
+	))
+time_it(do1, 'ioctl')
+time_it(do2, 'stty')
+time_it(do3, 'tput')
+time_it(do4, 'TERMCAP')
+#time_it(do5, 'LINES/COLUMNS')
