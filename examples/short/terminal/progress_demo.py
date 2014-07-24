fe3fedb73514cf355a2a6aefd25fc137a3af9c2b
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-24 11:22:13 +0300

    progressbar demo

---
+++ examples/short/terminal/progress_demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+An example of how to use python3-progressbar
+"""
+
+import progressbar # for ProgressBar
+import time # for sleep
+
+pbar = progressbar.ProgressBar()
+pbar.start()
+for i in range(10):
+	# do something
+	time.sleep(1)
+	pbar.update(i+1)
+#pbar.finish()
