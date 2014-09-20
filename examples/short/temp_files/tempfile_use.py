6e531ee910a7e7083cb6a8bdc403498f4d41af72
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-20 11:28:01 +0300

    improved the example

--- examples/short/temp_files/tempfile_use.py
+++ examples/short/temp_files/tempfile_use.py
@@ -8,7 +8,8 @@ Demo how to create a temp file in python
 
 import tempfile # for NamedTemporaryFile
 
-#t=tempfile.NamedTemporaryFile(delete=True)
-t=tempfile.NamedTemporaryFile()
+# delte=True is the default...
+#t=tempfile.NamedTemporaryFile()
+t=tempfile.NamedTemporaryFile(delete=False)
 
 print('name is [{0}]'.format(t.name))
