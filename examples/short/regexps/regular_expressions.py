a32b09eb9d9efdb6abb24049241b8c85fa3609f5
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-25 15:38:20 +0300

    improved the regular expression example

--- examples/short/regexps/regular_expressions.py
+++ examples/short/regexps/regular_expressions.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
-"""
-This is a sample for using regular expressions in python
+'''
+This is an example that demonstrates using regular expressions in python
+
+NOTES:
+- .match matches the *entire* string.
+- .findall can return all matches as strings.
 
 	Mark Veltzer <mark@veltzer.net>
-"""
+'''
 import re # for compile, finditer
 
 c=re.compile('^\tfoobar (\d+)\n$')
@@ -16,3 +20,7 @@ if m:
 	print('m.group(1) is [{g}]'.format(g=m.group(1)))
 else:
 	print('no match')
+
+c=re.compile('foobar \d+')
+l=c.findall('adfad foobar 20 sadfasd foobar 5 asdfasdfad foobar 3235')
+print(l)
