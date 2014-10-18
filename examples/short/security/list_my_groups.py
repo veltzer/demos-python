b1a86cf7a903d66fa5e24f215a09d6f4fd8e011f
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-19 02:40:42 +0300

    added another example

---
+++ examples/short/security/list_my_groups.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+This example is very similar to the command line groups(1) or id(1) utilities.
+
+References:
+http://stackoverflow.com/questions/9323834/python-how-to-get-group-ids-of-one-username-like-id-gn
+'''
+
+import grp # for getgrall, getgrgid
+import pwd # for getpwnam
+import getpass # for getuser
+
+########
+# code #
+########
+
+username=getpass.getuser()
+userid=pwd.getpwnam(username).pw_uid
+print('current username is [{0}] and uid is [{1}]'.format(username, userid))
+groups=[(g.gr_name, g.gr_gid) for g in grp.getgrall() if username in g.gr_mem]
+primary_gid=pwd.getpwnam(username).pw_gid
+details=(grp.getgrgid(primary_gid).gr_name, primary_gid)
+groups.append(details)
+print(groups)
