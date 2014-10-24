cd1371d72d3ece57758d6227920b10e6538d77c6
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-24 07:28:35 +0300

    removed some data files which were needed to run the demos

--- examples/short/iteration/group_by.py
+++ examples/short/iteration/group_by.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 
-import sys
-import itertools
+'''
+This is a demo of itertools.groupby
+'''
+
+import itertools # for groupby
 
 def indentation(line):
 	return len(line)-len(line.lstrip())
 
-for (indent, paragraph) in itertools.groupby(sys.stdin, key=indentation):
+data='''this is no indent 1
+this is no indent 2
+	this is 1 indent 1
+	this is 1 indent 2
+this is no indent 3
+this is no indent 4
+	this is 1 indent 3
+	this is 1 indent 4'''
+
+for (indent, paragraph) in itertools.groupby(data.split('\n'), key=indentation):
 	print('%d-spaced paragraph'%indent)
 	for line in paragraph:
 		print(line.strip())
--- examples/short/technology/smtplib_demo.py
+++ examples/short/technology/smtplib_demo.py
@@ -1,46 +1,47 @@
 #!/usr/bin/python3
 
 '''
-An example of an application sending an email.
+An example of an application sending an email. This uses smtplib which is a standard python library.
 '''
 
-import smtplib
-import email.mime.text
-import configparser
-import os
-import sys
+import smtplib # for SMTP
+import email.mime.text # for MIMEText
 
-def send_email():
-	debug=False
-	config=configparser.ConfigParser()
-	config.read(['myworld.cfg'])
-	p_subject=config.get('email','subject')
-	p_from=config.get('email','from')
-	p_to=config.get('email','to').split(',')
-	p_smtp_host=config.get('email','smtp_host')
-	p_content=config.get('email','content')
-	p_user=config.get('email','user')
-	p_password=config.get('email','password')
-	p_debug=bool(config.get('email','debug'))
-	p_usetls=bool(config.get('email','usetls'))
-	#p_nonexist=config.get('email','nonexist')
+def send_email(
+	subject='default subject',
+	fr='default from',
+	to=None,
+	content='default content',
+	smtp_host='localhost',
+	smtp_port=587,
+	usetls=True,
+	user=None,
+	password=None,
+	debug=False,
+	):
+	# build the message
+	msg=email.mime.text.MIMEText(content)
+	msg['Subject']=subject
+	msg['From']=fr
+	# send the message via our own SMTP server,but dont include the envelope header.
+	#server=smtplib.SMTP()
+	server=smtplib.SMTP(host=smtp_host, port=smtp_port)
+	server.ehlo()
+	#server.connect(host=smtp_host, port=smtp_port)
 	if(debug):
-		#output the entire config
-		config.write(sys.stdout)
-		p_debug=True
-	# build the message...
-	msg=email.mime.text.MIMEText(p_content)
-	msg['Subject']=p_subject
-	msg['From']=p_from
-	# Send the message via our own SMTP server,but dont include the # envelope header.
-	server=smtplib.SMTP(p_smtp_host)
-	if(p_debug):
 		server.set_debuglevel(1)
-	if(p_usetls):
+	if(usetls):
 		server.starttls()
-	server.login(p_user,p_password)
-	server.sendmail(p_from,p_to,msg.as_string())
+	server.login(user, password)
+	server.sendmail(fr, to, msg.as_string())
 	server.quit()
 
 if __name__=='__main__':
-	send_email()
+	send_email(
+		smtp_host='smtp.gmail.com',
+		user='mark.veltzer@gmail.com',
+		to='mark.veltzer@gmail.com',
+		fr='mark.veltzer@gmail.com',
+		password='XXXXXXXX',
+		debug=True,
+	)
