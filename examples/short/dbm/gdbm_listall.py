d19863bd7a976e8e52709421967f1ae61be9bbd8
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-01 06:08:07 +0200

    small fix

--- examples/short/dbm/gdbm_listall.py
+++ examples/short/dbm/gdbm_listall.py
@@ -8,6 +8,7 @@ standard python library
 import dbm.gnu # for open
 
 # the 'c' in the next row means open rw and create if it doesn't exist
+#d=dbm.gnu.open('/home/mark/.imap_import.db', 'c')
 d=dbm.gnu.open('/tmp/foo.gdbm', 'c')
 
 # show all key value pairs
