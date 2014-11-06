2c6afe030a9d46dce4b124a398b079d82057c371
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-06 18:02:09 +0200

    another example of ConfigParser

---
+++ examples/short/config/configparser_no_error_on_non_existing_files.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+This is an example that shows that ConfigParser does NOT complain about
+reading files which don't exist.
+Quite stupid.
+'''
+
+import os.path # for isfile
+import configparser # for ConfigParser
+
+c=configparser.ConfigParser()
+c.read('nonexit.ini')
