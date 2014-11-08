05d1b961fb746f1d461c81dc2708b840eed2d63b
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-08 14:02:23 +0200

    added deletion of environment variable example

--- examples/short/environment_variables/simple.py
+++ examples/short/environment_variables/simple.py
@@ -11,5 +11,10 @@ if 'HOSTNAME' in os.environ:
 	print(os.environ['HOSTNAME'])
 else:
 	print('you dont have a HOSTNAME in your environment, it is probably just a shell variable')
+
+# lets delete an environment variable
+del os.environ['USER']
+assert 'USER' not in os.environ
+
 for k, v in os.environ.items():
 	print(k, v)
