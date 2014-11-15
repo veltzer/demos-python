722a4d34fc100437b3282ed1edb2e6272c3fc6d5
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-15 20:36:15 +0200

    made this example better

--- examples/long/reading_a_static_file_from_inside_a_package/README.txt
+++ examples/long/reading_a_static_file_from_inside_a_package/README.txt
@@ -1,6 +1,9 @@
 This example shows how to read resources from a package in python.
 
-It uses the python module pkg_resources which IS NOT in the python standard library.
+There are three ways shown here:
+- python module pkg_resources which IS NOT in the python standard library.
+- own hand crafted code.
+- pkgutil which is in the standard library.
 
 References:
 http://stackoverflow.com/questions/6028000/python-how-to-read-a-static-file-from-inside-a-package
--- examples/long/reading_a_static_file_from_inside_a_package/mypack/mymod.py
+++ examples/long/reading_a_static_file_from_inside_a_package/mypack/mymod.py
@@ -1,4 +1,17 @@
 import pkg_resources
 
+import os.path # for dirname, join
+import pkgutil # for get_data
+
 static_file_content=pkg_resources.resource_string('mypack', 'static_file.html').decode()
 print('static_file_content is [{0}]'.format(static_file_content))
+
+def get_real_filename(filename):
+	return os.path.join(os.path.dirname(__file__), filename)
+def get_data(filename):
+	return open(get_real_filename(filename), 'rb').read()
+static_file_content2=get_data('static_file.html').decode()
+print('static_file_content2 is [{0}]'.format(static_file_content2))
+
+static_file_content3=pkgutil.get_data('mypack', 'static_file.html').decode()
+print('static_file_content3 is [{0}]'.format(static_file_content3))
