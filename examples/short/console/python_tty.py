484c7322af74d9ad404d8795b33d8e24758a1083
Mark Veltzer <mark.veltzer@gmail.com>
2015-04-08 16:35:56 +0300

    more code

---
+++ examples/short/console/python_tty.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+This example shows /dev/tty and standard streams.
+'''
+
+import sys # for stdout, stderr
+
+print('Hello from regular print')
+print('Hello from sys.stdout', file=sys.stdout)
+print('Hello from sys.stderr', file=sys.stderr)
+
+sys.stdout.close()
+
+# these will cause an exception
+#print('Where did this go?')
+#print('Where did this go?', file=sys.stdout)
+
+with open('/dev/tty', 'w') as f:
+	f.write('Hello from /dev/tty...\n')
+	f.flush()
