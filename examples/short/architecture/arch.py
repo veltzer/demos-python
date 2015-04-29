3f22609aaa3b06e927b3c853f42bc5c5cb7bef47
Mark Veltzer <mark.veltzer@gmail.com>
2015-04-30 02:09:03 +0300

    another example about architecture

---
+++ examples/short/architecture/arch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+This example shows how to find the current architecture
+'''
+
+import platform # for processor()
+
+print(platform.processor())
+#print(platform.architecture())
