0ce35fb8c48c1b66df5aa7d94bd0b31b87afd164
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-12 02:05:41 +0300

    more order in the exercises

---
+++ exercises/basic/text_processing/start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def right_align_to(lines, width):
+	'''Right-align all lines to given width.'''
+	result = []
+	for line in lines:
+		result.append(line.rjust(width))
+	return result
+
+for line in right_align_to(['foo', 'x', '12345678'], 8):
+	print(line)
--- exercises/check_ok.sh
+++ exercises/check_ok.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find . -mindepth 2 -type f -not -name "exercise.txt" -not -name "solution.py" -not -name "solution1.py" -not -name "solution2.py" -not -name "data.txt" -not -name "data.xml"
+find . -mindepth 2 -type f -not -name "exercise.txt" -not -name "solution.py" -not -name "solution1.py" -not -name "solution2.py" -not -name "data.txt" -not -name "data.xml" -not -name "start.py" -not -name "data1.txt" -not -name "data2.txt"
