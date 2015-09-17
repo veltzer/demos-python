6e0175be11a546059fdd785e8497c4a35fdf7a37
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-17 21:37:58 +0300

    added references

--- src/examples/short/random/shuffle.py
+++ src/examples/short/random/shuffle.py
@@ -2,6 +2,9 @@
 
 '''
 This example shows how to shuffle a list in python
+
+References:
+http://stackoverflow.com/questions/2124347/how-to-generate-permutations-of-array-in-python
 '''
 
 import random  # for seed, shuffle
