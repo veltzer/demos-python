66f978dea294a7fb40402b93134c9198c4491097
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-17 08:45:05 +0300

    better templates

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ version: 1
 Demos for the Python programming language
 
 This project explores the python programming language.
-The idea is that when you are programming you many find many examples
-over the internet but you always need to tweek them a bit to see
+The idea is that when you are programming you may find many examples
+over the Internet but you always need to tweek them a bit to see
 how you can get to an example which answers your needs precisely.
-Over the internet this is hard to do. But if you clone hundreds
-of exmples and you have them in working condition on your machine
+Over the Internet this is hard to do. But if you clone hundreds
+of examples and you have them in working condition on your machine
 you can just find the example which is closest to what you need
 and tweek it until it does exactly what you need. You may even
 add your example as a new example or contribute it back to this project.
 This is a much better way of programming since you are not doing
 the experiments on your full system but rather on a small example
-that is easy to run and build. Once you have the example ready and
-running correctly you can incorporate it into your official product.
+that is easy to run, debug and manage. Once you have the example ready and
+running correctly you can incorporate it's code into a larger project.
 
-	Mark Veltzer <mark@veltzer.net>, 2010-2014
+	Mark Veltzer <mark@veltzer.net>, 2010-2015
--- templardefs/project.py
+++ templardefs/project.py
@@ -7,18 +7,18 @@ def populate(d):
 	d.project_long_description='Demos for the Python programming language'
 	d.project_year_started='2010'
 	d.project_description='''This project explores the python programming language.
-The idea is that when you are programming you many find many examples
-over the internet but you always need to tweek them a bit to see
+The idea is that when you are programming you may find many examples
+over the Internet but you always need to tweek them a bit to see
 how you can get to an example which answers your needs precisely.
-Over the internet this is hard to do. But if you clone hundreds
-of exmples and you have them in working condition on your machine
+Over the Internet this is hard to do. But if you clone hundreds
+of examples and you have them in working condition on your machine
 you can just find the example which is closest to what you need
 and tweek it until it does exactly what you need. You may even
 add your example as a new example or contribute it back to this project.
 This is a much better way of programming since you are not doing
 the experiments on your full system but rather on a small example
-that is easy to run and build. Once you have the example ready and
-running correctly you can incorporate it into your official product.'''.format(**d)
+that is easy to run, debug and manage. Once you have the example ready and
+running correctly you can incorporate it's code into a larger project.'''.format(**d)
 
 def getdeps():
 	return [
