be623ec51e027994f1120a8ffa20e151b58b315b
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-17 21:34:02 +0300

    dont repeat ourselves

--- src/examples/short/random/shuffle.py
+++ src/examples/short/random/shuffle.py
@@ -6,7 +6,7 @@ This example shows how to shuffle a list in python
 
 import random  # for seed, shuffle
 # for deterministic behaviour
-random.seed(7)
+#random.seed(7)
 l = list(range(1, 10))
 random.shuffle(l)
 print(l)
