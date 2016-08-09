880272e1e46638ecd15caaa7fb6d9037d56c7dfd
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-09 15:31:45 +0300

    expanded this example

--- src/examples/short/json/json_store.py
+++ src/examples/short/json/json_store.py
@@ -2,6 +2,9 @@
 
 '''
 This example explores loading/saving dictionaries in python from/to files
+
+Notes:
+- python sets are not JSON serializable
 '''
 
 import json  # for dump, load
@@ -10,12 +13,19 @@ my_dict = {
     'one': 'onev',
     'two': 'twov',
 }
+my_set = {
+    'one',
+    'two',
+}
 
 filename = '/tmp/json_store.json'
 
 with open(filename, 'w') as f:
     json.dump(my_dict, f, indent=4)
+    #json.dump(my_set, f)
 
 with open(filename) as f:
     other_dict = json.load(f)
+    #other_set = json.load(f)
 print(other_dict)
+#print(other_set)
