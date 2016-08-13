e6af9c7a2cc7ba7307632524b4cbfb425929e4dc
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-13 05:19:01 +0300

    more examples

--- src/examples/short/strings/catenate_strings.py
+++ src/examples/short/strings/catenate_strings.py
@@ -4,10 +4,33 @@
 This example explores how best to catenate strings in python.
 
 The results are surprising.
+It seems the + operator is the quickest
+Join has pretty good performance.
 '''
 
 import timeit  # for timeit
 
-print(timeit.timeit('\'\'.join([\'a\', \'b\', \'c\'])', number=10000))
-print(timeit.timeit('s=\'a\'+\'b\'+\'c\'', number=10000))
-print(timeit.timeit('s=\'a\'; s+=\'b\'; s+=\'c\'', number=10000))
+x='abc'
+y='def'
+def concat1():
+    z=x+y
+    return z
+def concat2():
+    z = "%s%s" % (x, y) 
+    return z 
+def concat3(): 
+    z = "{}{}".format(x, y) 
+    return z 
+def concat4(): 
+    z = "{0}{1}".format(x, y) 
+    return z
+def concat5(): 
+    return ''.join([x,y])
+
+number=1000000
+
+print('+ operator', timeit.timeit(concat1, number=number))
+print('%s%s', timeit.timeit(concat2, number=number))
+print('{}{}', timeit.timeit(concat3, number=number))
+print('{0}{1}', timeit.timeit(concat4, number=number))
+print('join', timeit.timeit(concat5, number=number))
