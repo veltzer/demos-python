8653d64b06bedf06712147c63bd3e85b19656c8b
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-13 13:42:54 +0300

    better string performance example

--- src/examples/short/strings/catenate_strings_performance.py
+++ src/examples/short/strings/catenate_strings_performance.py
@@ -4,8 +4,9 @@
 This example explores how best to catenate strings in python.
 
 The results are surprising.
-It seems the + operator is the quickest
-Join has pretty good performance too.
+- It seems the + operator is the quickest
+- Join and %s%s come next.
+- Formatting is last.
 '''
 
 import timeit  # for timeit
@@ -15,22 +16,34 @@ y='def'
 def concat1():
     z=x+y
     return z
+concat1.name='+ operator'
 def concat2():
-    z = "%s%s" % (x, y) 
+    z = '%s%s' % (x, y) 
     return z 
+concat2.name='%s%s'
 def concat3(): 
-    z = "{}{}".format(x, y) 
+    z = '{}{}'.format(x, y) 
     return z 
+concat3.name='{}{}'
 def concat4(): 
-    z = "{0}{1}".format(x, y) 
+    z = '{0}{1}'.format(x, y) 
     return z
+concat4.name='{0}{1}'
 def concat5(): 
     return ''.join([x,y])
+concat5.name='join'
 
-number=1000000
+functions=[
+        concat1,
+        concat2,
+        concat3,
+        concat4,
+        concat5,
+]
 
-print('+ operator', timeit.timeit(concat1, number=number))
-print('%s%s', timeit.timeit(concat2, number=number))
-print('{}{}', timeit.timeit(concat3, number=number))
-print('{0}{1}', timeit.timeit(concat4, number=number))
-print('join', timeit.timeit(concat5, number=number))
+number=2000000
+
+results=[(timeit.timeit(f, number=number), f.name) for f in functions]
+sorted_results=sorted(results, key=lambda tup: tup[0])
+for r in sorted_results:
+    print('{0:.4f}: {1}'.format(r[0], r[1]))
