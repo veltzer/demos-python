e0f921ae7c9b9824fdb8108357593ce789a5367a
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-19 00:44:13 +0300

    incorporated some of udi orons notes

--- src/examples/short/logging/basic.py
+++ src/examples/short/logging/basic.py
@@ -2,9 +2,12 @@
 
 '''
 This is a basic logging example
+
+The idea of passing __name__ to the logger
+is that you can turn logs on and off from certain modules.
 '''
 
 import logging # for getLogger
 
-logger=logging.getLogger("example")
-logger.warning("this is a warning")
+logger=logging.getLogger(__name__)
+logger.warning("this is a warning"+__name__)
