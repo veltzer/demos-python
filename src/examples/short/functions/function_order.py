92badbaef017d8d56ca5ba631c1c4cfdf45bbb34
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-23 17:53:02 +0300

    improved this example

--- src/examples/short/functions/function_order.py
+++ src/examples/short/functions/function_order.py
@@ -6,21 +6,32 @@ This example explores corret function order in the source code.
 This means that a function cannot be used until after it has been defined.
 '''
 
-# this is wrong
-# foo()
-
+# this is wrong since foo is not defined
+try:
+    foo()
+except:
+    print('yep, this failed')
 
 def foo():
     print('this is foo')
     bar()
 
-# this is wrong...
-# foo()
-
+# this will call foo but will fail once foo tried to call bar
+try:
+    foo()
+except:
+    print('yep, this failed')
 
 def bar():
     print('this is bar')
 
+# this should work now that both 'foo' and 'bar' are defined
 foo()
+
 del bar
-foo()
+
+# now that 'bar' is no longer defined any call to 'foo' should fail
+try:
+    foo()
+except:
+    print('yep, this failed')
