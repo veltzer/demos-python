27a93c49f4670773b574d9920f407894e8ab26ea
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-24 15:33:24 +0300

    added a few examples

---
+++ src/examples/short/iteration/filter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+An example of how to use the 'filter' built-in python function.
+
+Notes:
+- filter is builtin. No need to import anything.
+'''
+
+for x in filter(lambda x: len(x)==3, { "abc", "abcd", "cbcdef","yui"}):
+    print(x)
--- src/examples/short/iteration/zip.py
+++ src/examples/short/iteration/zip.py
@@ -5,20 +5,22 @@ This example iterates two or more containers at the same time.
 
 Notes:
 - if the two lists are not of the same length then zip iterates
-until the shorter one (see second example).
+until the shorter one (see example below).
 - for python 2.7 zip is less efficient than izip since zip builds
 a list of all the tuples in advance while izip does not.
 - for python 2.7 izip is from itertools while zip is default
-- for python 3 zip has the efficiency of python2.7's izip so you
-don't need to think about any of this (yay python3!)
-
+- for python 3 izip is gone and zip has the efficiency of
+python2.7's izip so you don't need to think about any of this
+(yay python3!)
 '''
+
+import itertools # for zip_longest
 
 ll1 = [1, 2, 3, 4, 5]
 ll2 = [10, 11, 12, 13, 14]
 ll3 = [20, 21, 22, 23, 24]
 
-for (l1, l2) in izip(ll1, ll2):
+for (l1, l2) in zip(ll1, ll2):
     print(l1, l2)
 
 for (l1, l2, l3) in zip(ll1, ll2, ll3):
@@ -28,4 +30,7 @@ lll1 = [1, 2, 3]
 lll2 = [4, 5]
 
 for (x, y) in zip(lll1, lll2):
+    print(x, y)
+
+for (x, y) in itertools.zip_longest(lll1, lll2):
     print(x, y)
