1959aa5650af1c070d6de6fdb76ecd1df83a663d
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-25 14:38:46 +0300

    better example

--- src/examples/short/iteration/enumerate.py
+++ src/examples/short/iteration/enumerate.py
@@ -6,8 +6,14 @@ This example shows how to use the 'enumerate' iterator.
 Notes:
 - enumerate is supposed to be efficient both in python2.7
 and in python3 although I haven't checked that.
+- the two code samples below do the same but the second
+is more beautiful.
 '''
 
 l=['a', 'b', 'c']
+
+for i in range(len(l)):
+    print(i,l[i])
+
 for i,c in enumerate(l):
     print(i,c)
