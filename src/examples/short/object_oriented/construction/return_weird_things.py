623cb87687c1b19feebebe5961e40a0f910b0348
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-26 18:47:31 +0300

    much better

---
+++ src/examples/short/object_oriented/construction/return_weird_things.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+'''
+Trying to return weird things from the constructor
+
+Conclusions:
+- you can only return None or nothing (which is None)
+from a import constructor.
+- this helps you to get out early if you don't want to
+execute the rest of the constructor code.
+- you cannot return 'self' since the __init__ method is
+not really a constructor in object oriented terminology
+but rather an initializer function. The object on which
+it works is already alive and well and is already determined
+and the initializer function has no say in that matter.
+That is why it's return value is uninteresting (it is not
+even returned to the programmer).
+'''
+
+class A:
+    def __init__(self):
+        return 0
+class B:
+    def __init__(self):
+        return None
+class C:
+    def __init__(self):
+        return self
+class D:
+    def __init__(self):
+        return
+
+try:
+    a = A()
+except Exception as e:
+    print('yes, got the exception [{0}]...'.format(str(e)))
+b=B()
+try:
+    c=C()
+except Exception as e:
+    print('yes, got the exception [{0}]...'.format(str(e)))
+d=D()
---
+++ src/examples/short/object_oriented/construction/throw_exceptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+This example demostrates that you can throw exceptions
+in the middle of python "constructors" (which are
+actually initializers).
+
+The explanation is that in python the '__init__' in not
+really a constructor but rather a standard method to do
+post construction initialization. As such it does not
+have any isseu with throwing exceptions or calling methods
+that may throw exceptions.
+'''
+
+class A:
+    def __init__(self):
+        raise ValueError('this is my error')
+
+try:
+    a = A()
+except ValueError as ve:
+    print('yes, got the exception [{0}]...'.format(str(ve)))
