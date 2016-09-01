2c8c5fed8f9056869aef59b8ac2309378b503d12
Mark Veltzer <mark.veltzer@gmail.com>
2016-09-01 11:27:13 +0300

    explanation about when to use warnings

--- src/examples/short/warnings/warnings_demo.py
+++ src/examples/short/warnings/warnings_demo.py
@@ -2,6 +2,14 @@
 
 '''
 A demo of how warnings work in python.
+
+When should you issue a warning?
+If you are a piece of code and you notice something abnormal, that probably
+indicates a bug in the current running process or in the way your code is
+being used by the user, then issue a warning.
+
+You should only issue a warning if you can continue running despite the warning.
+If you cannot continue to run, raise an exception.
 '''
 
 import warnings  # for warn, simplefilter
