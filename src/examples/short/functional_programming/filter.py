e928481719d2c78281e9e176834ed7c09ab945ca
Mark Veltzer <mark.veltzer@gmail.com>
2016-09-13 11:36:21 +0300

    added a filter example

---
+++ src/examples/short/functional_programming/filter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+A basic example for the python built-in 'filter' function
+
+Notes:
+- the function 'filter' is built-in. No need to import anything.
+- filter is an iterator.
+'''
+
+# make an explicit list
+l=[x for x in filter(lambda x: x%2==1, range(10))]
+print(l)
+
+for x in filter(lambda x: x%2==1, range(10)):
+	print(x)
