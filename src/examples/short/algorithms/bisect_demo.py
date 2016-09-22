1f526ae5e7d7f400fc8b62957c100c71b1ed21c2
Mark Veltzer <mark.veltzer@gmail.com>
2016-09-22 15:25:48 +0300

    started algo demos

---
+++ src/examples/short/algorithms/bisect_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+This example shows how to use the python bisect module
+
+The problem with bisect is that it does not allow you to supply a function
+to apply. This means that to use it you must maintain your own array which
+contains only the values by which to look.
+'''
+
+import bisect # for bisect
+
+l=[1, 3, 4, 5, 71, 82]
+
+print(bisect.bisect_left(l, 60))
