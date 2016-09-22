f85372f81713d17e881a67a951ca65be7edbd7ce
Mark Veltzer <mark.veltzer@gmail.com>
2016-09-22 19:04:26 +0300

    csv write example

---
+++ src/examples/short/technology/csv_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+Example of how to use the 'csv' module to write csv files
+'''
+
+import csv # for writer
+
+data=[
+    ['first, element', 5, ],
+    ['second, element', 7, ],
+]
+with open('/tmp/output.csv', 'w') as csvfile:
+    w = csv.writer(csvfile)
+    for d in data:
+        w.writerow(d)
