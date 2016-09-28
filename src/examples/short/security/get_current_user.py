b443f385e1b69b8abd6bf42a0d317d305dfc69ca
Mark Veltzer <mark.veltzer@gmail.com>
2016-09-28 11:23:02 +0300

    various ways to get the current user

---
+++ src/examples/short/security/get_current_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+This example lists the various ways to get the current user name in python.
+
+References:
+- http://stackoverflow.com/questions/842059/is-there-a-portable-way-to-get-the-current-username-in-python
+'''
+
+import getpass  # for getuser
+import os # for getlogin, getuid, getuid
+import pwd # for getpwuid
+
+
+print(getpass.getuser())
+print(os.getlogin())
+print(pwd.getpwuid(os.getuid())[0])
