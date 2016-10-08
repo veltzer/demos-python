ffbf5ed670b961e355f684b5a67a66a52270a87c
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-08 18:41:52 +0300

    added performance example for list

---
+++ src/examples/short/lists/removing_first_element_performance.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+'''
+This example proves that adding to the start of a list, removing from the start of a list
+are very expensive operations.
+
+It also shows that the same operations on a 'collections.deque' data structure are very
+efficient.
+'''
+
+import timeit  # for timeit
+import collections # for deque
+
+def func1():
+    del l[0]
+func1.name='del list[0]'
+
+def func2():
+    l.pop(0)
+func2.name='list.pop(0)'
+
+def func3():
+    l.insert(0, 'newelem')
+func3.name='insert(0, newelement)'
+
+def func4():
+    del d[0] 
+func4.name='del deque[0]'
+
+def func5():
+    d.popleft()
+func5.name='deque.popleft()'
+
+def func6():
+    d.appendleft('newelem')
+func6.name='deque.appendleft(newelement)'
+
+l=list(range(1000000))
+d=collections.deque(range(1000000))
+
+functions=[
+    func1,
+    func2,
+    func3,
+    func4,
+    func5,
+    func6,
+]
+
+number=1000
+results=[(timeit.timeit(f, number=number), f.name) for f in functions]
+sorted_results=sorted(results, key=lambda tup: tup[0])
+for r in sorted_results:
+    print('{0:.4f}: {1}'.format(r[0], r[1]))
