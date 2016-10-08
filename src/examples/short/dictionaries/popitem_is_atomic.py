393e5e0f756995e5b86a1946f14df50dc8aae0f9
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-08 13:32:19 +0300

    another example from hettinger

---
+++ src/examples/short/dictionaries/popitem_is_atomic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+This example shows d.popitem()
+
+The main point is that this method is atomic and can be used
+to distribute values of a dictionary between threads or processess
+in a multi-threaded or multi-processed pythong application.
+'''
+
+d={
+    "one":1,
+    "two":2,
+    "three":3,
+    "four":4,
+}
+while d:
+    key, value = d.popitem()
+    print(key, '-->', value)
