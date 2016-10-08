2478b02cbdae6ecf666e883570b28a510aeeede8
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-08 17:07:49 +0300

    added named tuples example

--- src/examples/short/tuples/iterating_some_members_of_a_set_of_tuples.py
+++ src/examples/short/tuples/iterating_some_members_of_a_set_of_tuples.py
@@ -2,7 +2,7 @@
 
 '''
 Sometimes you want to iterate tuples and you dont want all members.
-Here is how you do it
+Here is how you do it.
 
 Notes:
 - the underscore is actually a variable name so its use has no bearing on performance.
@@ -10,6 +10,7 @@ Notes:
 - you can even use the value of the underscore since it is a regular variable.
 - it is however non-descriptive and on code review it will be evident that you are not interested
 in that position within the tuple.
+- note that you cannot do something like 'for (x,,y) in tuples' as it will not compile
 
 References:
 http://stackoverflow.com/questions/3061336/how-can-i-iterate-over-only-the-first-variable-of-a-tuple
@@ -17,9 +18,9 @@ http://stackoverflow.com/questions/3061336/how-can-i-iterate-over-only-the-first
 
 tuples = [
     (1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
-        (10, 11, 12),
+    (4, 5, 6),
+    (7, 8, 9),
+    (10, 11, 12),
 ]
 
 for (x, _, y) in tuples:
---
+++ src/examples/short/tuples/named_tuples.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+Here is how to create and use named tuples in python.
+
+Refrences:
+- https://docs.python.org/3/library/collections.html#collections.namedtuple
+'''
+
+import collections # for namedtuple
+
+Point=collections.namedtuple('Point', ['x','y'])
+print(type(Point))
+print(dir(Point))
+print(Point._fields)
+p1=Point(x=5, y=6)
+print(p1)
+print(type(p1))
+p2=Point(5, 6)
+print(p2)
+print(type(p2))
+p3=Point(5, y=6)
+print(p3)
+print(type(p3))
+(x,y)=p3
+print(x,y)
+print(p3.x, p3.y)
+print(Point._make([5,6]))
+print(Point._source)
