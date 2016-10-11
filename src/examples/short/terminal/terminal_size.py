10e93c49bc0b9db7c204db8066564690a0a4bf2b
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-11 08:43:42 +0300

    improved two examples

--- src/examples/short/terminal/terminal_size.py
+++ src/examples/short/terminal/terminal_size.py
@@ -13,26 +13,26 @@ import os  # for popen, environ
 import fcntl  # for ioctl
 import struct  # for unpack, pack
 import termios  # for TIOCGWINSZ
-import time  # for time
+import timeit  # for timeit
 
 
 def terminal_size1():
-    h, w, hp, wp = struct.unpack('HHHH',
-                                 fcntl.ioctl(0, termios.TIOCGWINSZ,
-                                             struct.pack('HHHH', 0, 0, 0, 0)),
-                                 )
+    h, w, hp, wp = struct.unpack('HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)),)
     return w, h
+terminal_size1.name='ioctl'
 
 
 def terminal_size2():
     ret = os.popen('stty size', 'r').read().split()
     return int(ret[1]), int(ret[0])
+terminal_size2.name='stty'
 
 
 def terminal_size3():
     x = int(os.popen('tput cols', 'r').read())
     y = int(os.popen('tput lines', 'r').read())
     return x, y
+terminal_size3.name='tput'
 
 
 def terminal_size4():
@@ -42,59 +42,32 @@ def terminal_size4():
             key, val = tc_entry.split('#')
             d[key] = val
     return int(d['co']), int(d['li'])
-# this function does not work since LINES and COLUMNS are not exported
+terminal_size4.name='TERMCAP'
 
 
 def terminal_size5():
+    ''' this function does not work since LINES and COLUMNS are not exported'''
     x = int(os.environ['LINES'])
     y = int(os.environ['COLUMNS'])
     return x, y
+terminal_size5.name='environment'
 
 print(terminal_size1())
 print(terminal_size2())
 print(terminal_size3())
 print(terminal_size4())
-# print(terminal_size5())
+#print(terminal_size5())
 
-# lets compare performance
-count = 1000
+functions=[
+    terminal_size1,
+    terminal_size2,
+    terminal_size3,
+    terminal_size4,
+#    terminal_size5,
+]
 
-
-def do1():
-    for x in range(count):
-        terminal_size1()
-
-
-def do2():
-    for x in range(count):
-        terminal_size2()
-
-
-def do3():
-    for x in range(count):
-        terminal_size3()
-
-
-def do4():
-    for x in range(count):
-        terminal_size4()
-
-
-def do5():
-    for x in range(count):
-        terminal_size5()
-
-
-def time_it(f, desc):
-    time_before = time.time()
-    f()
-    time_after = time.time()
-    print('time taken for {desc}: {diff:.3f} seconds'.format(
-        diff=time_after - time_before,
-            desc=desc,
-    ))
-time_it(do1, 'ioctl')
-time_it(do2, 'stty')
-time_it(do3, 'tput')
-time_it(do4, 'TERMCAP')
-# time_it(do5, 'LINES/COLUMNS')
+number=1000
+results=[(timeit.timeit(f, number=number), f.name) for f in functions]
+sorted_results=sorted(results, key=lambda tup: tup[0])
+for r in sorted_results:
+    print('{0:.4f}: {1}'.format(r[0], r[1]))
