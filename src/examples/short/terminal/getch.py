b7d03dbc8d435543f01cf9429bb1a278b547adea
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-18 04:11:59 +0300

    added a getch example for terminals

---
+++ src/examples/short/terminal/getch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+This example shows how to read just one character from the keyboard in python
+
+References:
+- http://stackoverflow.com/questions/27750536/python-input-single-character-without-enter
+'''
+
+import termios # for tcgetattr, tcsetattr, TCSADRAIN
+import sys # for stdin
+import tty # for setraw
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+print('press 6 to end')
+while True:
+    c=getch()
+    print(c, end='')
+    sys.stdout.flush()
+    if c=='6':
+        break
