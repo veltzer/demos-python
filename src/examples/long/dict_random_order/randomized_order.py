41fc57abfc08310d461c76c7cf71a5c311302573
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-18 15:25:22 +0300

    improved the example further

--- src/examples/long/dict_random_order/randomized_order.py
+++ src/examples/long/dict_random_order/randomized_order.py
@@ -4,7 +4,20 @@ import os
 
 '''
 This example shows that iterating a hash produces random order
+
+NOTES:
+- with 'PYTHONHASHSEED' environment variable we can control the behavior
+of the hash.
+- 'PYTHONHASHSEED' does not guarantee traversal of elements in the
+dict will be in order of insertion. It does guarantee that the order will be
+the same in every run. So it guarantees determinism, not order which is
+sometimes good enough.
 '''
 
+print("No PYTHONHASHSEED");
+os.system('./sample.py')
+os.system('./sample.py')
+os.environ['PYTHONHASHSEED']="1"
+print("PYTHONHASHSEED=1");
 os.system('./sample.py')
 os.system('./sample.py')
