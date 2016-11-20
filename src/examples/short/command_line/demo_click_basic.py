b47722b09c041a4aaac37b371b15771fd45f7707
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-20 16:32:25 +0200

    better example for click

--- src/examples/short/command_line/demo_click_basic.py
+++ src/examples/short/command_line/demo_click_basic.py
@@ -11,10 +11,13 @@ import click
 
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
+@click.option('--name', prompt='Your name', help='The person to greet.')
+@click.option('--required', required=True, help='this is a required parameter')
+@click.option('--output', required=True, help='output file', type=click.File('w', lazy=False))
+def hello(count, name, required, output):
     """Simple program that greets NAME for a total of COUNT times."""
+    print(required)
+    print(output)
     for x in range(count):
         click.echo('Hello %s!' % name)
 
