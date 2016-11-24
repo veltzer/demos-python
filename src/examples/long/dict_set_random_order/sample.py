85fe12e264d878d7237cbb6f3290d1acc5808dd4
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-24 15:41:41 +0200

    more notes about dict and set random order

--- src/examples/long/dict_set_random_order/sample.py
+++ src/examples/long/dict_set_random_order/sample.py
@@ -8,10 +8,14 @@ import collections # for OrderedDict
 
 ud=dict()
 od=collections.OrderedDict()
+s=set()
 for i in range(10):
     ud[str(i)]=i
     od[str(i)]=i
+    s.add(str(i))
 print('unordered dict')
 print(' '.join(['{k},{v}'.format(k=k,v=v) for k,v in ud.items()]))
 print('ordered dict')
 print(' '.join(['{k},{v}'.format(k=k,v=v) for k,v in od.items()]))
+print('set')
+print(' '.join([x for x in s]))
