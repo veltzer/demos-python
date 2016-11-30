9b296edb7d1fa9f8d23aa9dd5dbe00f85cf0bae5
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-30 19:18:59 +0200

    mako issued and type hints

--- src/examples/long/templating_mako/simple.mako
+++ src/examples/long/templating_mako/simple.mako
@@ -20,3 +20,11 @@ http://stackoverflow.com/questions/18624290/how-to-check-if-a-key-exists-in-mako
 % else:
 	'a' is not in d
 % endif
+
+next line will not be shown since lines starting with ## are a comment in mako templates
+
+## this is a comment
+
+next line will be shown...
+
+<%text>## this is a comment</%text>
---
+++ src/examples/short/typing/get_type_hints.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+An example of how to extract type hints from a class
+"""
+
+from typing import get_type_hints
+from typing import ClassVar
+
+class Data:
+    #a: ClassVar[int] = 0
+    def doit(a: float) -> float:
+        return a+1
+
+def func(a: int) -> int:
+    return a+1
+
+import __main__
+a = 1 # type: int
+print(get_type_hints(__main__))
+
+#print(dir(a))
+
+
+#print(Data.__dict__)
+#print(Data)
+for x in get_type_hints(Data.__class__):
+    print(x)
+
+for x in get_type_hints(func):
+    print(x)
