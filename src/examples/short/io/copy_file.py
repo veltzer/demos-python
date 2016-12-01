1677afd2f108bc9f6fcebb847ff015e29e4c9690
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-01 16:31:15 +0200

    copy file example

---
+++ src/examples/short/io/copy_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to copy a file in python
+
+This example is based on shutil.copyfileobj which shuffles
+data from one file handle to another.
+"""
+
+import os.path
+import os
+import shutil
+
+
+source_file = '/etc/passwd'
+target_file = '/tmp/passwd'
+
+
+if os.path.isfile(target_file):
+    os.unlink(target_file)
+with open(source_file) as source_file_handle:
+    with open(target_file, 'w') as target_file_handle:
+        shutil.copyfileobj(source_file_handle, target_file_handle)
