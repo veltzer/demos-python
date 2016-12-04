7afbfaaf09bd64fbd526f583d4f846d28e7c2cc2
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-04 16:35:12 +0200

    added islice example

---
+++ src/examples/short/iteration/islice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to use itertools.islice to cut down on the number
+of iteration that a loop does...
+"""
+
+import sys
+from itertools import islice
+
+for i in islice(range(1000), sys.maxsize):
+    print(i)
