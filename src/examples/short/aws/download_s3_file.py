a4d862aa17fe504a493691e32a7862a520c929fe
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-04 11:53:53 +0200

    added example of downloading a file from s3

---
+++ src/examples/short/aws/download_s3_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This is an example of how to download a single file from s3
+"""
+
+import boto3
+import codecs
+
+do_count_lines = True
+bucket_name = 'twiggle-click-streams'
+key = 'catalogs/flipkart/catalog/2016-11-23/compressed/details.txt'
+local = '/tmp/details.txt'
+
+s3 = boto3.resource('s3')  # type: boto3.resources.factory.s3.ServiceResource
+bucket = s3.Bucket('twiggle-click-streams')  # type: boto3.resources.factory.s3.Bucket
+bucket.download_file(key, local)
