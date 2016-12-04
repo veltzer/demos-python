f547eb0d3c773c86e846a7f5cc24c381cb08c473
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-04 10:58:12 +0200

    example of using a multithreading pool

---
+++ src/examples/short/multi_core/pool_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to use a multiprocessing pool
+"""
+
+import multiprocessing  # for cpu_count
+import time  # for sleep
+
+def sleep_a_little(num):
+    print('before', num)
+    time.sleep(1)
+    print('after', num)
+
+pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+for i in range(10):
+    pool.apply_async(sleep_a_little, [i])
+pool.close()
+pool.join()
