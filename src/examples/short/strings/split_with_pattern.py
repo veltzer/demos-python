69c4e50fec4ff804cea1de34ff5804ed104070c8
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-05 11:45:36 +0200

    added advanced split with re example

---
+++ src/examples/short/strings/split_with_pattern.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+"""
+
+import re
+
+s = 'there , is, a house in new, orleans 100,000,000  ,0000  456, 3.1415  this.is a sentence. and'
+
+r = re.compile(r'(?<!\d),(?!\d)')
+
+re_comma = r'(,)(?!\d)|(?<!\d)(,)'
+re_dots = r'(\.)(?!\d)|(?<!\d)(\.)'
+re_space = r'\s'
+
+re_full = '|'.join([re_comma, re_dots, re_space])
+
+l = re.split(re_full, s)
+l = [ x for x in l if x]
+print(l)
