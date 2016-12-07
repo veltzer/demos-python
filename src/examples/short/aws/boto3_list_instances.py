df3b7312e097dfe48e4d196227bf061dd9654c2b
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-07 13:51:42 +0200

    added list intances example for boto3

---
+++ src/examples/short/aws/boto3_list_instances.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This is an example of how to list instances using the python boto3 library
+
+References:
+- http://boto3.readthedocs.io/en/latest/guide/migrationec2.html
+"""
+
+import boto3
+
+ec2 = boto3.resource('ec2')  # type: boto3.resources.factory.ec2.ServiceResource
+instances = ec2.instances.filter(
+    Filters=[
+        {'Name': 'instance-state-name', 'Values': ['running']},
+        {'Name': 'tag:Owner', 'Values': ['mark@twiggle.com']},
+    ]
+)
+for instance in instances:
+    print(instance.id, instance.instance_type)
