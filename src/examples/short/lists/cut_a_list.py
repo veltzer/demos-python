e6011cb2a48e47cd24ccc542bb591efc0110e0dc
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-07 11:46:51 +0200

    more stuff

---
+++ src/examples/short/lists/cut_a_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to cut a list in python
+
+References:
+- http://stackoverflow.com/questions/1534939/how-to-trim-a-list-in-python
+"""
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+other_list = my_list[:4]
+
+print(other_list)
