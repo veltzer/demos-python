97fcc47244a4f309987ee52b89c874045625b10d
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-10 12:53:11 +0200

    better example

--- src/examples/short/temp_files/tempfile_use.py
+++ src/examples/short/temp_files/tempfile_use.py
@@ -2,12 +2,23 @@
 
 """
 Demo how to create a temp file in python
+
+References:
+- https://docs.python.org/3/library/tempfile.html
 """
 
 import tempfile  # for NamedTemporaryFile
+import os.path  # for isfile
 
-# delte=True is the default...
-# t=tempfile.NamedTemporaryFile()
-t = tempfile.NamedTemporaryFile(delete=False)
+# delete=True is the default...
+with tempfile.NamedTemporaryFile(mode="wt") as t:
+    name = t.name
+    print('name is [{0}]'.format(t.name))
+    print("hello, world!", file=t)
+assert not os.path.isfile(name)
 
-print('name is [{0}]'.format(t.name))
+with tempfile.NamedTemporaryFile(mode="wt", delete=False) as t:
+    name = t.name
+    print('name is [{0}]'.format(t.name))
+    print("hello, world!", file=t)
+assert os.path.isfile(name)
