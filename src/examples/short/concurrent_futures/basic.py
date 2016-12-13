8cc2badab241adb6ce4f3e85038dc5c252b46a88
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-13 09:47:08 +0200

    simplify the example

--- src/examples/short/concurrent_futures/basic.py
+++ src/examples/short/concurrent_futures/basic.py
@@ -10,31 +10,19 @@ References:
 """
 
 import concurrent.futures
-import math
 
-PRIMES = [
-    112272535095293,
-    112582705942171,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    1099726899285419
-]
 
-def is_prime(n):
-    if n % 2 == 0:
-        return False
+def work(n):
+    for i in range(n):
+        pass
+    return i
 
-    sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    return True
 
 def main():
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
-            print('%d is prime: %s' % (number, prime))
+        for r in executor.map(work, [100000000]*8):
+            print(r)
+
 
 if __name__ == '__main__':
     main()
