5f5f0f5555375698c4f13ae9cf28c4a16809de58
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-19 12:51:39 +0200

    pickling demo is in

---
+++ src/examples/short/serialization/pickle_demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Demo of the 'pickle' module for serialization and deserialization
+"""
+
+import pickle
+
+# .p is for 'pickle'
+filename = '/tmp/serialized.p'
+
+favorite_color = { "lion": "yellow", "kitty": "red" }
+
+with open(filename, "wb") as file_handle:
+    pickle.dump(favorite_color, file_handle)
+
+with open(filename, "rb") as file_handle:
+    new_dict = pickle.load(file_handle)
+
+assert favorite_color == new_dict
