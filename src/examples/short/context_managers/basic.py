8a490e32d2d34200516673927eac8d65a7d04d8c
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-27 11:58:39 +0200

    more examples of context managers

--- src/examples/short/context_managers/basic.py
+++ src/examples/short/context_managers/basic.py
@@ -4,8 +4,11 @@
 This is an example of creating your own resource to be used with the 'with'
 python syntax. This is called a 'Context Manager'.
 
-TODO:
-- show how to use the return value of __enter__
+You can see the basic flow of code:
+- first the constructor of your context manager is called.
+- then your __enter__ code is called.
+- then the users code block is executed.
+- then your __exit__ code is called.
 """
 
 
@@ -14,47 +17,14 @@ class MyResource:
     exitCallsCounter = 0
 
     def __init__(self, suppress=False):
-        ''' this is the constructor (the thing we call at the 'with') '''
-        self.suppress = suppress
+        print("constructor")
 
     def __enter__(self):
-        ''' this method will be called at the begining of the 'with' block '''
-        print('doing __enter__')
-        MyResource.enterCallsCounter += 1
-        return self
+        print("enter")
 
     def __exit__(self, itype, value, traceback):
-        ''' this method will be called at the end of the 'with' block '''
-        print('doing __exit__', itype, value, traceback)
-        MyResource.exitCallsCounter += 1
-        return self.suppress
+        print("exit")
 
-''' first lets try to just see if enter and exit are called '''
+print("lets see a basic use of the context manager...")
 with MyResource() as r:
-    print(r)
-    print('in block of code')
-assert MyResource.enterCallsCounter == 1
-assert MyResource.exitCallsCounter == 1
-print(
-    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
-
-''' now lets try to throw an exception from the 'with' block and see that exit it called '''
-try:
-    with MyResource() as r:
-        print(r)
-        raise Exception('foobar')
-except Exception:
-    pass
-assert MyResource.enterCallsCounter == 2
-assert MyResource.exitCallsCounter == 2
-print(
-    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
-
-''' now lets try to suppress the exception thrown '''
-with MyResource(suppress=True) as r:
-    print(r)
-    raise Exception('foobar')
-assert MyResource.enterCallsCounter == 3
-assert MyResource.exitCallsCounter == 3
-print(
-    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
+    print("block of code")
---
+++ src/examples/short/context_managers/suppress_exceptions.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""
+This is an example of creating your own resource to be used with the 'with'
+python syntax. This is called a 'Context Manager'.
+
+NOTES:
+- the retrun value of __exit__ is whether or not to suppress an exception
+if such has happened.
+
+TODO:
+- show how to use the return value of __enter__
+"""
+
+
+class MyResource:
+    enterCallsCounter = 0
+    exitCallsCounter = 0
+
+    def __init__(self, suppress=False):
+        ''' this is the constructor (the thing we call at the 'with') '''
+        self.suppress = suppress
+
+    def __enter__(self):
+        ''' this method will be called at the begining of the 'with' block '''
+        print('doing __enter__')
+        MyResource.enterCallsCounter += 1
+        return self
+
+    def __exit__(self, itype, value, traceback):
+        ''' this method will be called at the end of the 'with' block '''
+        print('doing __exit__', itype, value, traceback)
+        MyResource.exitCallsCounter += 1
+        return self.suppress
+
+''' first lets try to just see if enter and exit are called '''
+with MyResource() as r:
+    print(r)
+    print('in block of code')
+assert MyResource.enterCallsCounter == 1
+assert MyResource.exitCallsCounter == 1
+print(
+    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
+
+''' now lets try to throw an exception from the 'with' block and see that exit it called '''
+try:
+    with MyResource() as r:
+        print(r)
+        raise Exception('foobar')
+except Exception:
+    pass
+assert MyResource.enterCallsCounter == 2
+assert MyResource.exitCallsCounter == 2
+print(
+    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
+
+''' now lets try to suppress the exception thrown '''
+with MyResource(suppress=True) as r:
+    print(r)
+    raise Exception('foobar')
+assert MyResource.enterCallsCounter == 3
+assert MyResource.exitCallsCounter == 3
+print(
+    'yes,if we got this far it means that both enter and exit were called exactly one time during the last attempt')
