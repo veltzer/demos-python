12c9419c8543b9c4d28006b2fb0f6b202182147a
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-09 10:26:08 +0200

    added better example for logging

---
+++ src/examples/long/logging/code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import logging.config
+
+logging.config.fileConfig('logging.conf')
+# logger = logging.getLogger(__name__)
+logger = logging.getLogger('simple')
+
+print(__name__)
+logger.debug("this is a debug message")
+logger.info("this is a info message")
