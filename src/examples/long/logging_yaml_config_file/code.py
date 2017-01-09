539093a0bb4cd58399e6dc59d3a15dd45dd710f2
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-09 16:58:43 +0200

    example of python logging with yaml

---
+++ src/examples/long/logging_yaml_config_file/code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import logging.config
+import yaml
+
+"""
+This is how to load a yaml file as configuration for python logging
+"""
+with open('logging.yaml') as f:
+    config = yaml.load(f.read())
+    logging.config.dictConfig(config)
+
+logger = logging.getLogger('simple')
+
+print(__name__)
+logger.debug("this is a debug message")
+logger.info("this is a info message")
