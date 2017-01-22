2d2d7034c5edfa8f4473188036571781c19d63f1
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-22 13:35:38 +0200

    more code

---
+++ src/examples/short/generators/generator_into_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This example shows that if you have a generator
+you can turn it into a real list using several ways:
+- list()
+- [] (list comprehension)
+
+It also shows that the () comprehension will leave a generator
+as a generator and the wrong ways of doing it...
+"""
+
+def generate_items():
+    for i in range(10):
+        yield i
+
+y = list(generate_items())
+print(y)
+
+z = [x for x in generate_items()]
+print(z)
+
+# wrong examples
+w1 = (x for x in generate_items())
+print(w1)
+
+w2 = (generate_items())
+print(w2)
+
+w3 = [generate_items()]
+print(w3)
