2f415d7149d54cb95174b19f9f5b6d614e374153
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-22 13:20:32 +0200

    added itemgetter example

---
+++ src/examples/short/iteration/itemgetter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to use operator.itemgetter to avoid writing
+small lambda functions to achieve the same goal.
+
+References:
+http://stackoverflow.com/questions/22412258/get-the-first-element-of-each-tuple-in-a-list-in-python
+"""
+
+from operator import itemgetter
+
+rows = [(1, 2), (3, 4), (5, 6)]
+print(list(map(itemgetter(1), rows)))
