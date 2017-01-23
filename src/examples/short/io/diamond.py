340a69c1c0db42cb69bd51824ed4fa4c10c9df1a
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-23 15:18:06 +0200

    added diamon example

---
+++ src/examples/short/io/diamond.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to approximate the <> operator of perl
+in python.
+
+Reminder: the <> operator (or as it's called diamond) reads
+all line seither specified on the command line or from standard input.
+"""
+
+import sys
+
+def diamond_lines():
+    if len(sys.argv)==1:
+        for line in sys.stdin.readlines():
+            yield line
+    else:
+        for filename in sys.argv[1:]:
+            with open(filename, 'rt') as file_handle:
+                 for line in file_handle:
+                     yield line
+
+for line in diamond_lines():
+    print(line, end='')
