6507efb288310f7d9a32be51eb10f30fd7cd068d
Mark Veltzer <mark.veltzer@gmail.com>
2017-01-26 14:13:28 +0200

    more code

---
+++ src/examples/short/closure/closure_modify_primitive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This is an example that shows that you cannot change primitives in closures.
+"""
+
+
+def make_adder():
+    x = 0
+    def adder():
+        x += 1
+        print(x)
+    return adder
+
+f = make_adder()
+f()
+f()
+f()
