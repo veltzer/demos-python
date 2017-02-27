16f7792aec965fd409795da25190e3bd0a21f83d
Mark Veltzer <mark.veltzer@gmail.com>
2017-02-27 14:33:46 +0200

    added benchmark of list, tuple, set and frozenset

---
+++ src/examples/short/performance/list_vs_tuple_vs_set_vs_frozenset.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This is an example of comparing the performance of list vs tuple vs
+set vs frozenset
+"""
+
+import timeit
+import time
+
+n = 1000
+repetitions = 1
+l = [str(k) for k in range(n)]
+s = set(l)
+f = frozenset(l)
+t = tuple(l)
+
+def demo_list():
+    return [value in l for value in l]
+
+def demo_set():
+    return [value in s for value in l]
+
+def demo_frozenset():
+    return [value in f for value in l]
+
+def demo_tuple():
+    return [value in t for value in l]
+
+print("list {:.04f}".format(timeit.timeit(demo_list, number=repetitions)))
+print("set {:.04f}".format(timeit.timeit(demo_set, number=repetitions)))
+print("frozenset {:.04f}".format(timeit.timeit(demo_frozenset, number=repetitions)))
+print("tuple {:.04f}".format(timeit.timeit(demo_tuple, number=repetitions)))
