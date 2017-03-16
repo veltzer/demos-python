eb5c587f139ae6a21290f31ee0fca714b7cc3b28
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-16 17:06:08 +0200

    added more examples

---
+++ src/examples/short/lists/selecting_specific_items_from_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This is an example of how to select specific fields from a list
+"""
+
+fields = [ 3, 4 ]
+
+data = [1, 2, 3, 4, 5, 6, 7]
+
+selection = [data[x] for x in fields]
+
+print(selection)
