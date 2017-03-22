b22c83dd17e008a57126afd1b7ee635a6bc7835c
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-22 22:37:08 +0200

    more demos about reading full file

--- src/examples/short/io/read_full_file.py
+++ src/examples/short/io/read_full_file.py
@@ -3,6 +3,8 @@
 """
 This example shows how to read the full content of a file in python
 
+In order to read the entire file you must open it in binary mode.
+
 References:
 - http://stackoverflow.com/questions/7409780/reading-entire-file-in-python
 """
---
+++ src/examples/short/io/read_full_file_binary_vs_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to read the full content of a file in python
+
+In order to read the entire file you must open it in binary mode.
+
+References:
+- http://stackoverflow.com/questions/7409780/reading-entire-file-in-python
+"""
+
+# Create file of 256 bytes
+with open('/tmp/testfile', 'wb') as fout:
+    fout.write(''.join(map(chr, range(256))).encode())
+
+# Text mode
+with open('/tmp/testfile') as fin:
+    print('Opened in text mode is:', len(fin.read()))
+    # Opened in text mode is: 26
+
+# Binary mode - note 'rb'
+with open('/tmp/testfile', 'rb') as fin:
+    print('Opened in binary mode is:', len(fin.read()))
+    # Opened in binary mode is: 256
