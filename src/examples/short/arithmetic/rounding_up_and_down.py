739adc70f6a05e65340bad0a92a848d6bbe89a7b
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-26 17:19:56 +0300

    ceil and floor examples

---
+++ src/examples/short/arithmetic/rounding_up_and_down.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to round up and down
+
+NOTES:
+- in python2 the 'ceil' and 'floor' class methods of math return float types
+and not ints as in python3. python3 is saner.
+
+References:
+- http://stackoverflow.com/questions/2356501/how-do-you-round-up-a-number-in-python
+"""
+
+import math
+
+x = math.ceil(6.25)
+print(type(x), x)
+
+x = math.floor(6.75)
+print(type(x), x)
