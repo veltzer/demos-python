615ac3904cb9850d9e3e6d2d0e0b1a5b22092113
Mark Veltzer <mark.veltzer@gmail.com>
2017-03-27 18:20:56 +0300

    another good example

---
+++ src/examples/short/lists/split_a_list_into_evenly_sized_chunks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to split a list into evenly sized chunks.
+
+References:
+- http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+"""
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+for l in chunks(range(5,28), 7):
+    print(l)
