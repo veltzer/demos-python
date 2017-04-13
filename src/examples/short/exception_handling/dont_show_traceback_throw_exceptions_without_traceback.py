45bd6718373f03e7a4479207f3ae561b323fc0a9
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-13 18:43:03 +0300

    another no traceback example

---
+++ src/examples/short/exception_handling/dont_show_traceback_throw_exceptions_without_traceback.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This example explores how to inhibit python from printing the stack trace when
+exiting from an exception.
+
+References:
+- http://stackoverflow.com/questions/17784849/in-python-how-do-i-print-an-error-message-without-printing-a-traceback-and-clos
+"""
+
+import sys
+import inspect
+
+class NoTraceBackWithLineNumber(Exception):
+    def __init__(self, msg):
+        try:
+            ln = sys.exc_info()[-1].tb_lineno
+        except AttributeError:
+            ln = inspect.currentframe().f_back.f_lineno
+        self.args = "{0.__name__} (line {1}): {2}".format(type(self), ln, msg),
+        sys.exit(self)
+
+class MyNewError(NoTraceBackWithLineNumber):
+    pass
+
+raise MyNewError("Now TraceBack Is Gone")
