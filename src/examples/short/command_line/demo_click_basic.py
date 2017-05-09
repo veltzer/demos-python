0c3dccdf5985ea3e8590f5882d721cc0f9712681
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-09 17:15:39 +0300

    added another python example

--- src/examples/short/command_line/demo_click_basic.py
+++ src/examples/short/command_line/demo_click_basic.py
@@ -3,6 +3,11 @@
 """
 A Demo for click
 
+Notes:
+- when you give a boolean flag the "--XXX" could be written as "--XXX/--YYY" and then
+    --XXX will turn on the flag and --YYY will turn it off.
+    In this case *YOU MUST NOT* pass the 'type=bool' parameters (bug in click)
+
 References:
 - http://click.pocoo.org/5
 """
@@ -16,6 +21,7 @@ import click
 @click.option('--required', required=True, help='this is a required parameter')
 @click.option('--output', required=True, help='output file', type=click.File('w', lazy=False))
 @click.option('--hash-type', required=True, type=click.Choice(['md5', 'sha1']))
+@click.option('--foo/--bar', required=False, help="this is foo")
 def hello(count, name, required, output, hash_type):
     """Simple program that greets NAME for a total of COUNT times."""
     print(required)
---
+++ src/examples/short/lists/remove_element_from_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to remove certain elements from a list.
+
+References:
+- http://stackoverflow.com/questions/2793324/is-there-a-simple-way-to-delete-a-list-element-by-value
+"""
+
+my_list = [1, 2, 3, 4, 2, 6, 7, 2]
+# This just removes the first occurence
+my_list.remove(2)
+print(my_list)
+# This removes all occurences
+my_list = [x for x in my_list if x != 2]
+print(my_list)
