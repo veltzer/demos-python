b67b7f83e99e08eaac16acbca7cb789d3dfcc540
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-09 11:33:53 +0300

    another example

---
+++ src/examples/short/lists/add_at_begining_of_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to add an element at the begining of a list in python.
+
+References:
+- http://stackoverflow.com/questions/17911091/append-integer-to-beginning-of-list-in-python
+"""
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+my_list.insert(0, -1)
+print(my_list)
