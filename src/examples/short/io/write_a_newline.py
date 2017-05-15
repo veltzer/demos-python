4c11ebec58cd3ec48f45279697796235cdba2b32
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-15 13:27:51 +0300

    another example

---
+++ src/examples/short/io/write_a_newline.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to correctly write a newline charcter
+in python in a way which is operating system portable.
+
+References:
+- http://stackoverflow.com/questions/1223289/how-to-write-native-newline-character-to-a-file-descriptor-in-python
+"""
+
+import os
+
+print("Hello World", end="")
+print(os.linesep, end="")
