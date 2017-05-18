d4b52c8382896d1ba02295f9aefa91000ef16e72
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-18 16:02:34 +0300

    two more examples

--- src/examples/short/lmdb/simple.py
+++ src/examples/short/lmdb/simple.py
@@ -14,8 +14,20 @@ with env.begin(write=True) as txn:
     k=bytes('hello','utf8')  # Data on disk is a byte array...
     v=bytes('world','utf8')  # ...so explicitly state the encoding!
 
-    txn.put(k,v)                   # read
-    v=txn.get(k,default='what??')  # write
+    txn.put(k,v)                   # write
+    v=txn.get(k,default='what??')  # read
 
+    v=str(v,'utf8')          # again: Explicitly state the encoding!
+    assert v == "world"
+
+env=lmdb.open('/tmp/file.mdb', subdir=False, max_dbs=2)
+with env.begin(write=True, db="a") as txn:
+    k=bytes('a_key','utf8')  # Data on disk is a byte array...
+    v=bytes('a_val','utf8')  # ...so explicitly state the encoding!
+    txn.put(k,v)                   # write
+print(env)
+
+with env.begin(write=False, db="a") as txn:
+    v=txn.get("a_key")  # read
     v=str(v,'utf8')          # again: Explicitly state the encoding!
     print(v)
---
+++ src/examples/short/syntax/c_style_braces.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This example shows that python will not let you use curly braces
+instead of tab for scope demarcation.
+
+References:
+- http://stackoverflow.com/questions/21125228/where-is-from-future-import-braces-code
+"""
+
+from __future__ import braces
