a0e619e333a9ce4acb378a862f5283b977e97b5c
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-18 16:11:19 +0300

    and another fix

--- src/examples/short/syntax/c_style_braces.py
+++ src/examples/short/syntax/c_style_braces.py
@@ -4,8 +4,11 @@
 This example shows that python will not let you use curly braces
 instead of tab for scope demarcation.
 
+Notes:
+- the line is currently commented out because it causes a syntax error.
+
 References:
 - http://stackoverflow.com/questions/21125228/where-is-from-future-import-braces-code
 """
 
-from __future__ import braces
+# from __future__ import braces
