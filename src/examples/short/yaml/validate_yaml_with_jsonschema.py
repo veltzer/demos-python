30b6f46100108c4caae5b06ec455809ad418a71f
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-24 14:33:11 +0300

    added yaml validation example

---
+++ src/examples/short/yaml/validate_yaml_with_jsonschema.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+An example of how to validate a YAML document using the jsonschema module.
+
+References:
+- https://stackoverflow.com/questions/3262569/validating-a-yaml-document-in-python
+"""
+
+from jsonschema import validate
+import yaml
+
+schema = """
+type: object
+properties:
+  testing:
+    type: array
+    items:
+      enum:
+        - this
+        - is
+        - a
+        - test
+"""
+
+good_instance = """
+testing: ['this', 'is', 'a', 'test']
+"""
+
+validate(yaml.load(good_instance), yaml.load(schema))  # passes
+
+# Now let's try a bad instance...
+
+bad_instance = """
+testing: ['this', 'is', 'a', 'bad', 'test']
+"""
+
+try:
+    validate(yaml.load(bad_instance), yaml.load(schema))
+except Exception as e:
+    print(e)
+    exit(1)
