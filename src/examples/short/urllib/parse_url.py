2e3b1974b3077e166dfa55a2b65e2590ee598e44
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-29 14:31:08 +0300

    demo of parsing a URL

---
+++ src/examples/short/urllib/parse_url.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This is a basic example of how to parse urls
+"""
+
+import urllib.parse
+import sys
+
+url = "https://www.wish.com/search/living%20room#default"
+o = urllib.parse.urlparse(url)
+qs = urllib.parse.parse_qs(o.query)
+path = o.path
+start = "/search/"
+if not path.startswith(start):
+    print("fail")
+    sys.exit(1)
+path = path[len(start):]
+print(urllib.parse.unquote(path))
