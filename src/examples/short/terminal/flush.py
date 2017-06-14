e9ae0d480494d659aceece9841feed83812ea6cd
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-14 11:56:20 +0300

    another example

---
+++ src/examples/short/terminal/flush.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to use flush
+
+Notes:
+- there is no way that I know of not to end a print with a newline and flush using
+only the 'print' function.
+- this means you do need to add the 'sys.stdout.flush()' to see things.
+"""
+
+import sys
+import time
+
+
+print("Hello", end="")
+time.sleep(5)
+sys.stdout.flush()
--- src/examples/short/tqdm/while_loop_know_limit.py
+++ src/examples/short/tqdm/while_loop_know_limit.py
@@ -15,10 +15,10 @@ counter = 0
 with tqdm.tqdm(total=limit) as pbar:
     while True:
         r = random.randrange(0, 100)
-        pbar.update(r)
         counter += r
         if counter > limit:
             break
+        pbar.update(r)
 
 # and without the 'with' syntax:
 pbar = tqdm.tqdm(total=limit)
