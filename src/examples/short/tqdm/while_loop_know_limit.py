335272fe4bac3d596c8706f1334591b1518fbd70
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-14 13:06:00 +0300

    more examples

--- src/examples/short/tqdm/while_loop_know_limit.py
+++ src/examples/short/tqdm/while_loop_know_limit.py
@@ -3,6 +3,11 @@
 """
 This is how to use tqdm when you know number of iterations.
 
+Notes:
+- in the first context manager method you must take care to update the progress
+bar *on every piece of work you do* so that the progress bar will get to 100%
+at the end of the work.
+
 References:
 - https://pypi.python.org/pypi/tqdm
 """
@@ -11,16 +16,23 @@ import tqdm
 import random
 
 limit = 100000000
+
+# example with a context manager
 counter = 0
 with tqdm.tqdm(total=limit) as pbar:
     while True:
         r = random.randrange(0, 100)
+        pbar.update(r)
         counter += r
         if counter > limit:
             break
-        pbar.update(r)
 
-# and without the 'with' syntax:
+# example of not updating correctly
+with tqdm.tqdm(total=limit) as pbar:
+    pbar.update(1000)
+pbar.close()
+
+# example without a context manager
 pbar = tqdm.tqdm(total=limit)
 counter = 0
 while True:
@@ -29,4 +41,5 @@ while True:
     counter += r
     if counter > limit:
         break
+# you must add this to close
 pbar.close()
