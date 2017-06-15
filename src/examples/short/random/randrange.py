31539824176ed5208ea7916211fc0c722272331d
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-15 19:10:50 +0300

    new demo

---
+++ src/examples/short/random/randrange.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This is a simple example of how to get random numbers in python.
+
+Randrange can be used with just stop, start/stop, or start/stop/step
+parameters.
+
+References:
+- https://docs.python.org/3/library/random.html
+"""
+
+import random
+
+for i in range(10):
+    print(random.randrange(10))
+print("=========")
+for i in range(10):
+    print(random.randrange(15, 20))
+print("=========")
+for i in range(10):
+    print(random.randrange(-20, -10, 2))
