8558d6a1340ef257a94a0b4fe9aeffa58a297668
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-03 16:00:07 +0300

    fix the example

--- src/examples/short/gdbm/query.py
+++ src/examples/short/gdbm/query.py
@@ -5,13 +5,30 @@ This is an example of how to query the values in a gdbm file.
 """
 
 import dbm.gnu
+import os.path
 
 filename = "/tmp/test.gdbm"
+
+if os.path.isfile(filename):
+    os.unlink(filename)
+
+# lets put in some data
+d = dbm.gnu.open(filename, 'n')
+d["ehad"] = "yes!"
+d.close()
+
+# lets open for read
 # the 'r' in the next line means open for read only
 d = dbm.gnu.open(filename, 'r')
 
+
 print('ehad in db', 'ehad' in d)
 print('one in db', 'one' in d)
-print(d["one"])
+
+# lets query a key which isn't there
+try:
+    print(d["one"])
+except KeyError as e:
+    print("yes!, got KeyError")
 
 d.close()
