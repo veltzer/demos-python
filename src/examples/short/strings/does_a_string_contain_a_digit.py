c67778f1b2c36275aceb554a02c6a308c8ef4a5f
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-06 20:09:35 +0300

    another example in python

---
+++ src/examples/short/strings/does_a_string_contain_a_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This example shows how to find whether a string contains a digit fast.
+
+References:
+- https://stackoverflow.com/questions/17681655/python-if-a-word-contains-a-digit
+"""
+
+def has_digits(word: str) -> bool:
+    return any(ch.isdigit() for ch in word)
+
+print(has_digits("b747"))
+print(has_digits("alpha"))
