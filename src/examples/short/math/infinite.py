0e62366ea4e66ee26828692958a04489c6508e7e
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-06 13:20:06 +0300

    infinite in python

---
+++ src/examples/short/math/infinite.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+How to use infinite in python.
+
+Note that math.inf is intimately connected with math.nan since, for instance,
+math.inf/math.inf=math.nan
+
+References:
+- https://stackoverflow.com/questions/7781260/how-can-i-represent-an-infinite-number-in-python
+"""
+
+
+import math
+
+print(math.inf)
+print(-math.inf)
+print(-math.inf/2)
+print(math.inf*2)
+print(math.inf-math.inf)
+print(math.inf/math.inf)
+print(math.nan)
