ae46c2adf34bd88a3a44da438e2ea338da4fc81f
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-11 18:37:44 +0300

    added cmd2 quit example

---
+++ src/examples/short/cmd2/quit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+An example of how to build a quit command in cmd2.
+The idea is just to "return True" from the command.
+
+Note that the 'do_quit' function still needs to get
+the argument 'arg' as it is called with two arguments
+(self and arg).
+"""
+
+from cmd2 import Cmd
+
+class CmdLineApp(Cmd):
+    def __init__(self):
+        Cmd.__init__(self, use_ipython=False)
+
+    def do_quit(self, arg):
+        """ quit the app """
+        return True
+
+if __name__ == '__main__':
+    c = CmdLineApp()
+    c.cmdloop()
