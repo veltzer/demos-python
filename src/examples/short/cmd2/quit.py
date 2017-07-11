8c77d68d5447262ea75bc8cffa4f20154ffbb65a
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-11 18:38:19 +0300

    reference

--- src/examples/short/cmd2/quit.py
+++ src/examples/short/cmd2/quit.py
@@ -7,6 +7,9 @@ The idea is just to "return True" from the command.
 Note that the 'do_quit' function still needs to get
 the argument 'arg' as it is called with two arguments
 (self and arg).
+
+References:
+- https://stackoverflow.com/questions/15537427/how-to-exit-the-cmd-loop-of-cmd-module-cleanly
 """
 
 from cmd2 import Cmd
