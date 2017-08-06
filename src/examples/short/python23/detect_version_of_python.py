1e4bf324760bdd4f126eb4cc130f0ba37b3128e7
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-06 17:42:58 +0300

    better example

--- src/examples/short/python23/detect_version_of_python.py
+++ src/examples/short/python23/detect_version_of_python.py
@@ -5,11 +5,17 @@ This script shows how to detect which version of python you are running in.
 
 References:
 - http://sweetme.at/2013/10/21/how-to-detect-python-2-vs-3-in-your-python-script/
+- https://stackoverflow.com/questions/9079036/detect-python-version-at-runtime
 """
 
 import sys
 
-if sys.version_info > (3, 0):
+if sys.version_info >= (3, 0):
     print("you are in python 3")
 else:
     print("you are in python 2")
+
+def is_2():
+    return sys.version_info[0]==2
+
+print(is_2())
