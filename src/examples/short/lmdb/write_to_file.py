969e583227b147ccdb9af7e03b3021b5c5ee304b
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-09 13:22:03 +0300

    simple writing example

---
+++ src/examples/short/lmdb/write_to_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+This is how to work with an lmdb file.
+"""
+
+import lmdb
+
+env=lmdb.open('/tmp/file.mdb', subdir=False, map_size=1000000000000)
+with env.begin(write=True) as txn:
+    k=bytes('hello','utf8')
+    v=bytes('world','utf8')
+    txn.put(k,v)
+    v=txn.get(k,default='what??')
+    v=str(v,'utf8')
+    assert v == "world"
