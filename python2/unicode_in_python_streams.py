f4428f87c8fe9c7ceb73ce944b39513045fee4d8
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-15 11:38:45 +0300

    another example of unicode

---
+++ python2/unicode_in_python_streams.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+"""
+This example shows that if you are using python2 then you must
+decorate your file handles with codecs.getreader and codecs.getwriter
+if you want to read and write in 'unicode' type instead of 'str' type.
+"""
+
+import sys
+import codecs
+
+filename = "/etc/passwd"
+
+handle = open(filename, mode="r")
+handle = codecs.getreader(encoding="utf-8")(handle)
+line = handle.readline()
+assert type(line)==unicode
+
+handle = open(filename, mode="r")
+line = handle.readline()
+assert type(line)==str
