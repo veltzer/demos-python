cf884e24f8f698a921eb964cca49f9abfa64463b
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-24 16:12:11 +0300

    python2 forked off

--- src/examples/short/modules/using_imp.py
+++ src/examples/short/modules/using_imp.py
@@ -12,6 +12,7 @@ import imp
 # noinspection PyDeprecation
 imp.load_source('__main__', 'using_imp_one.py')
 print('add(2,2) is', add(2, 2))
+# noinspection PyDeprecation
 imp.load_source('__main__', 'using_imp_two.py')
 print('add(2,2) is', add(2, 2))
 
@@ -19,5 +20,6 @@ print('add(2,2) is', add(2, 2))
 import math
 
 print('math.sin(0.5) is', math.sin(0.5))
+# noinspection PyDeprecation
 imp.load_source('math', 'using_imp_three.py')
 print('math.sin(0.5) is', math.sin(0.5))
--- src/examples/short/systems_programming/popen_streaming.py
+++ src/examples/short/systems_programming/popen_streaming.py
@@ -6,6 +6,6 @@ This example shows how to call popen and get the return text.
 
 import os
 
-pout = os.popen('./demo_process.py', buffering=1)
+pout = os.popen('./demo_process.py', mode="r")
 for line in pout:
     print('line is', line, end='')
