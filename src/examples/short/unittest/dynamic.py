f2298b6587da80258b7f77d8179220663c4e990a
Mark Veltzer <mark.veltzer@gmail.com>
2017-09-11 18:50:00 +0300

    dynamic tests

---
+++ src/examples/short/unittest/dynamic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to create a test suite object with dynamically
+created tests.
+
+References:
+- https://kite.com/examples/python/4920
+"""
+
+import unittest
+
+def test_generator(a):
+    def test():
+        assert a % 2 == 0
+    return test
+
+suite = unittest.TestSuite()
+test_cases = [2, 4]
+for case in test_cases:
+    suite.addTest(
+        unittest.FunctionTestCase(
+            test_generator(case)))
+
+
+unittest.TextTestRunner().run(suite)
