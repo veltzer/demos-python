52bfb385cfbcf0c8322e2648c77b4c0df54c9018
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-15 12:45:52 +0300

    more examples

---
+++ src/examples/short/exception_handling/catch_more_than_one_exception_type.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to catch more than one exception type in python.
+
+The syntax is:
+    except <tuple of exception types> as e:
+"""
+
+def raise_it(a_type):
+    raise a_type('hello')
+
+for x in ValueError, RuntimeError:
+    try:
+        raise_it(x)
+    except (ValueError, RuntimeError) as e:
+        print(type(e))
---
+++ src/examples/short/exception_handling/raise_without_parameters.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+This example explores what happens when you just 'raise' without parameters.
+
+The difference is that when you 'raise' with an exception the place where
+you raise is added to the stack trace of the exception. If this is a new
+exception that you create
+"""
+
+# you cannot just raise without a prior exception being active
+try:
+    raise
+except RuntimeError as e:
+    print('All is ok. you cannot raise without a prior active exception [{}]'.format(e))
+
+def traceback_len(tb):
+    count = 0
+    while tb.tb_next:
+        count += 1
+        tb = tb.tb_next
+    return count
+
+# lets try to raise without an exception
+try:
+    try:
+        raise ValueError('hello')
+    except ValueError as e:
+        raise
+except ValueError as e:
+    tb = e.__traceback__
+    assert traceback_len(tb)==0
+
+# lets try to raise with an exception
+try:
+    try:
+        raise ValueError('hello')
+    except ValueError as e:
+        raise e
+except ValueError as e:
+    tb = e.__traceback__
+    assert traceback_len(tb)==1
