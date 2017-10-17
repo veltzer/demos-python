4cdc8e9abf5c0f4984ba691ef7633a3e51d69ec6
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-17 16:42:35 +0300

    more exapmles

---
+++ src/examples/short/temp_files_and_directories/create_temporary_directory_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to create a temporary directory in python and
+destroy it's content once you are done with it.
+
+References:
+- https://stackoverflow.com/questions/3223604/how-to-create-a-temporary-directory-and-get-the-path-file-name-in-python
+"""
+
+import tempfile
+import shutil
+import os.path
+
+dirpath = tempfile.mkdtemp()
+print("dirpath is [{}]".format(dirpath))
+shutil.copy("/etc/passwd", os.path.join(dirpath, "passwd"))
+shutil.rmtree(dirpath)
