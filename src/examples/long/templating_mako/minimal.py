636b1966ee15d214c1f07fca111443b7cd303e02
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-30 19:14:31 +0200

    minimal example

---
+++ src/examples/long/templating_mako/minimal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import datetime
+import mako.template
+
+
+def years(x):
+    curr_year = datetime.datetime.now().year
+    return ','.join(map(str, range(x, curr_year + 1)))
+
+
+d = {
+    'a': 'b',
+    'c': 'd',
+}
+
+input_filename = 'src/examples/long/templating_mako/simple.mako'
+output_filename = '/tmp/simple'
+
+template = mako.template.Template(filename=input_filename)
+output = template.render(foo='bar', years=years, d=d)
+with open(output_filename, 'w') as file_handle:
+    file_handle.write(output)
--- src/examples/long/templating_mako/with_lookup.py
+++ src/examples/long/templating_mako/with_lookup.py
@@ -16,27 +16,27 @@ d = {
     'c': 'd',
 }
 
-if len(sys.argv) != 3:
-    print(sys.argv[0] + ': usage: ' + sys.argv[
-        0] + ' [input] [output]', file=sys.stderr)
-    sys.exit(1)
-
 input_encoding = 'utf-8'
 output_encoding = 'utf-8'
-p_input = sys.argv[1]
-p_output = sys.argv[2]
+p_input = 'src/examples/long/templating_mako/simple.mako'
+p_output = '/tmp/simple'
 
 mylookup = mako.lookup.TemplateLookup(
-    directories=['.'], input_encoding=input_encoding, output_encoding=output_encoding)
+    directories=['.'],
+    input_encoding=input_encoding,
+    output_encoding=output_encoding,
+)
 template = mako.template.Template(
-    filename=p_input, lookup=mylookup, output_encoding=output_encoding, input_encoding=input_encoding)
+    filename=p_input,
+    #lookup=mylookup,
+    #output_encoding=output_encoding,
+    #input_encoding=input_encoding,
+)
 # the order here is importat. Do not open the file for writing until the rendering
 # is complete since we want to make sure that the rendering process does not throw
 # an exception. Only then do we open the file for writing.
 # otherwise we will leave behind a partially written file and force the user
 # to fix his makefile to remove the cruft we left behind...
 output = template.render(foo='bar', years=years, d=d)
-file = open(p_output, 'wb')
-file.write(output)
-file.close()
-# os.chmod(p_output, 0o0444)
+with open(p_output, 'w') as file_handle:
+    file_handle.write(output)
