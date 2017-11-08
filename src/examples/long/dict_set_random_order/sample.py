b5cb09e5a22954ee82e2beed5aad9574232bc303
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-08 19:09:40 +0200

    more for this sample

--- src/examples/long/dict_set_random_order/sample.py
+++ src/examples/long/dict_set_random_order/sample.py
@@ -5,11 +5,22 @@ This is an example which shows a stadard dictionary vs an ordered dict.
 """
 
 import collections
+import os
+import sys
 
+d = dict((L, i) for i, L in enumerate('abcd'))
+print(d)
+sys.exit(0)
+d = dict()
+for x in dir(os):
+    d[x] = None
+# d = {x:None for x in dir(os)}
+print(d)
+sys.exit(0)
 ud = dict()
 od = collections.OrderedDict()
 s = set()
-for i in range(10):
+for i in range(100):
     ud[str(i)] = i
     od[str(i)] = i
     s.add(str(i))
