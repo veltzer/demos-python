53f0c91d5be8dc52a52b7372a4f2d4eb66d831d6
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-18 15:25:59 +0200

    added traceback printing

--- src/examples/short/exception_handling/dont_show_traceback_excepthook.py
+++ src/examples/short/exception_handling/dont_show_traceback_excepthook.py
@@ -11,6 +11,7 @@ References:
 """
 
 import sys
+import traceback
 
 
 def do_error():
@@ -23,6 +24,7 @@ def do_error():
 def excepthook(_type, value, _traceback):
     # this loop will drill to the core of the problem
     # use only if this is what you want to show...
+    # traceback.print_exception(value=value, tb=_traceback, etype=_type)
     while value.__cause__:
         value = value.__cause__
     print(value)
