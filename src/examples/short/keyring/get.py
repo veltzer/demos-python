548da509da90c9d7127d8b1c1fa9eb8dea9f101c
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-25 07:14:22 +0200

    examples for keyring

---
+++ src/examples/short/keyring/get.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+A basic example of how to get a password from the default
+keyring of you environment.
+"""
+
+import keyring
+password = keyring.get_password(u"dummyapp", u"mark.veltzer@gmail.com")
+print("your password is [{}]".format(password))
---
+++ src/examples/short/keyring/set.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""
+This is an example of setting a password.
+"""
+
+import keyring
+keyring.set_password("dummyapp", "mark.veltzer@gmail.com", "thisisthepassword")
