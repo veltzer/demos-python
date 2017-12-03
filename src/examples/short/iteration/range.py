1ec49ea2de3c00b395551ca48f8e36b2d5e2de95
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-03 13:16:07 +0200

    more notes

--- src/examples/short/iteration/range.py
+++ src/examples/short/iteration/range.py
@@ -11,5 +11,33 @@ while 'xrange' only iterated the given list.
 'xrange' and so you don't have to worry about any of these.
 """
 
+print('range(10)')
 for i in range(10):
+    print(i)
+print('range(5, 10)')
+for i in range(5, 10):
+    print(i)
+print('range(2, 10, 3)')
+for i in range(2, 10, 3):
+    print(i)
+print('range(11, 2, -3)')
+for i in range(11, 2, -3):
+    print(i)
+
+# lets write our own range function
+def simple_range(stop, start=0, step=1):
+    count = start 
+    while count < stop:
+        yield count
+        count +=  step
+    raise StopIteration()
+
+print('simple_range(10)')
+for i in simple_range(10):
+    print(i)
+print('simple_range(5, 10)')
+for i in simple_range(5, 10):
+    print(i)
+print('simple_range(2, 10, 3)')
+for i in simple_range(2, 10, 3):
     print(i)
