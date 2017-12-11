909ad10cdf2020448bbb05c5591158639b83433b
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-11 11:49:55 +0200

    added another example

--- src/examples/short/iteration/chunk.py
+++ src/examples/short/iteration/chunk.py
@@ -8,11 +8,22 @@ one producing N pieces of data (except possibly the last one).
 
 The result is that the best option is to use the iterator
 API directly (look at the function 'chunk' below).
+
+Regarding checking if something is a generator.
+There are two ways to check this:
+- using types.GeneratorType:
+    import types
+    assert isinstance(x, types.GeneratorType)
+- using inspect:
+    import inspect
+    inspect.isgeneratorfunction(x)
+it seems that the second catches more than the second and that it
+what we use here.
 """
 
 import itertools
-import types
 import timeit
+import inspect
 
 def chunk_groupby(data, n):
     return ((d for _, d in dd) for (_, dd) in itertools.groupby(enumerate(data), key=lambda v: v[0] // n))
@@ -45,30 +56,40 @@ def chunk(data, n):
     while not over.is_cancelled:
         yield return_n()
 
-for d in chunk_groupby(range(100), 7):
-    assert isinstance(d, types.GeneratorType)
-    print(list(d))
+range_limit = 100
+jump = 7
+count = 0
+for d in chunk_groupby(range(range_limit), jump):
+    inspect.isgeneratorfunction(d)
+    assert list(d) == list(range(count, min(count + jump, range_limit)))
+    count += jump
 
-for d in chunk_groupby_closure(range(100), 7):
-    #print(type(d))
-    #assert isinstance(d, types.GeneratorType)
-    print(list(d))
+count = 0
+for d in chunk_groupby_closure(range(range_limit), jump):
+    inspect.isgeneratorfunction(d)
+    assert list(d) == list(range(count, min(count + jump, range_limit)))
+    count += jump
 
-for d in chunk(range(100), 7):
-    assert isinstance(d, types.GeneratorType)
-    print(list(d))
+count = 0
+for d in chunk(range(range_limit), jump):
+    inspect.isgeneratorfunction(d)
+    assert list(d) == list(range(count, min(count + jump, range_limit)))
+    count += jump
 
 # lets compare the performance
+range_limit = 1000000
+jump = 1000
 def func_chunk_groupby():
-    for d in chunk_groupby(range(1000000), 1000):
+    for d in chunk_groupby(range(range_limit), jump):
         _ = list(d)
 def func_chunk_groupby_closure():
-    for d in chunk_groupby_closure(range(1000000), 1000):
+    for d in chunk_groupby_closure(range(range_limit), jump):
         _ = list(d)
 def func_chunk():
-    for d in chunk(range(1000000), 1000):
+    for d in chunk(range(range_limit), jump):
         _ = list(d)
 
-print(timeit.timeit(func_chunk_groupby, number=10))
-print(timeit.timeit(func_chunk_groupby_closure, number=10))
-print(timeit.timeit(func_chunk, number=10))
+how_much = 10
+print(timeit.timeit(func_chunk_groupby, number=how_much))
+print(timeit.timeit(func_chunk_groupby_closure, number=how_much))
+print(timeit.timeit(func_chunk, number=how_much))
---
+++ src/examples/short/lists/compare_two_ordered_lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to compare two orderd lists in python.
+The full description is that we want the lists to be of the same length
+and that every element in position x in the first list should be equal
+to element in position x in the second list.
+
+The short answer is to use the '==' operator for list.
+
+References:
+- https://stackoverflow.com/questions/36420022/how-can-i-compare-two-ordered-lists-in-python
+"""
+
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+
+assert list1, list2
