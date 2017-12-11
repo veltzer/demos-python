046cdafad1bd131d14871b6937afba5a364c6430
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-11 12:08:33 +0200

    another distinction

---
+++ src/examples/short/generators/generator_function_vs_generator_object.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+This example shows the differece between a generator function and a
+generator object.
+
+generator function: a function which is a generator (has yields).
+    - can be detected using inspect.isgeneratorfunction(f)
+generator object: a generator function which has been called once.
+    - can be detected using isinstance(f, types.GeneratorType)
+
+References:
+- https://stackoverflow.com/questions/6416538/how-to-check-if-an-object-is-a-generator-object-in-python
+"""
+
+import inspect
+import types
+
+def generator_function():
+    yield 1
+    yield 2
+    yield 3
+
+print(type(generator_function))
+assert inspect.isgeneratorfunction(generator_function)
+assert not isinstance(generator_function, types.GeneratorType)
+
+f = generator_function()
+print(type(f))
+assert not inspect.isgeneratorfunction(f)
+assert isinstance(f, types.GeneratorType)
