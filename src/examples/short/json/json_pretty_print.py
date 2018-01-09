8562357f8737fc90870d5947b6e87347f65cceb2
Mark Veltzer <mark.veltzer@gmail.com>
2018-01-09 14:30:22 +0200

    added another example

---
+++ src/examples/short/json/json_pretty_print.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to pretty print json using the 'json' module.
+
+References:
+- https://stackoverflow.com/questions/12943819/how-to-prettyprint-a-json-file
+"""
+
+import json
+import sys
+
+my_dict = {
+    'one': 'one value',
+    'two': 'two value',
+}
+
+print(json.dumps(my_dict, indent=4, sort_keys=True))
+# or
+json.dump(my_dict, sys.stdout, indent=4, sort_keys=True)
