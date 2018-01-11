64489bfcd1c7adc1555128090e76a301427702e4
Mark Veltzer <mark.veltzer@gmail.com>
2018-01-11 16:36:12 +0200

    better example

--- src/examples/short/enum/basic.py
+++ src/examples/short/enum/basic.py
@@ -12,4 +12,9 @@ class SampleEnum(enum.Enum):
     two = 2
     three = 3
 
+# this is how to get the enum value (which is not the same as the int or the string)
 print(type(SampleEnum.one))
+# this is how to get the int value
+print(type(SampleEnum.one.value))
+# this is how to get the string name
+print(type(SampleEnum.one.name))
