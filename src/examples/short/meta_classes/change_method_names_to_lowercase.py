5c41bc044ced67fbf0ce8fafab056b4668b191e9
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-04 13:35:09 +0200

    notes

--- src/examples/short/meta_classes/change_method_names_to_lowercase.py
+++ src/examples/short/meta_classes/change_method_names_to_lowercase.py
@@ -2,6 +2,12 @@
 
 """
 This is an example of how to use meta classes to change all class methods to lowercase.
+The most important things to remember about writing meta classes:
+- override '__new__' when you want to override the class creation.
+- override '__init__' when you want to intervene after class creation.
+
+References:
+- https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses/
 """
 class MetaClass(type):
     def __new__(mcs, name, bases, cls_dict):
