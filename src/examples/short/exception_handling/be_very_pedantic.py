b2f136b053d72fe9c25aaeed63513b4067e7a08b
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-20 18:13:11 +0200

    another example

---
+++ src/examples/short/exception_handling/be_very_pedantic.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to catch all exceptions, and exit the software in any case.
+"""
+
+import sys
+import traceback
+
+
+def excepthook(_type, value, _traceback):
+    # this loop will drill to the core of the problem
+    # use only if this is what you want to show...
+    # traceback.print_exception(value=value, tb=_traceback, etype=_type)
+    while value.__cause__:
+        value = value.__cause__
+    print("An exception was throw... Refusing to continue...")
+    sys.exit(1)
+
+sys.excepthook = excepthook
+
+raise ValueError('core')
+
+# simulate a piece of bad code which "swallows" exceptions...
+try:
+    raise ValueError('core')
+except Exception as e:
+    print("haha, got the exception and continuing anyway...")
+print("after")
