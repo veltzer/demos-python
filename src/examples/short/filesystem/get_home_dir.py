8525304eb403cd16fd4bc906680427681205f813
Mark Veltzer <mark.veltzer@gmail.com>
2019-07-26 12:57:52 +0300

    more examples

--- src/examples/short/filesystem/get_home_dir.py
+++ src/examples/short/filesystem/get_home_dir.py
@@ -7,3 +7,9 @@ This is an example of how to get the home directory in python
 import os
 
 print(os.getenv('HOME'))
+
+from os.path import expanduser
+print(expanduser("~"))
+
+from pathlib import Path
+print(Path.home())
