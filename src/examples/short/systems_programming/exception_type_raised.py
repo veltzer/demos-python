78429a8217e505d6f2302f45199e95063b6cf610
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-26 11:08:38 +0300

    another example

---
+++ src/examples/short/systems_programming/exception_type_raised.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+This example investigates which error is raised when a child fails.
+
+The result is that the exception type is 'subprocess.CalledProcessError'.
+"""
+
+import subprocess
+
+try:
+    subprocess.check_call(['ls', 'nonexistant'], stderr=subprocess.DEVNULL)
+except subprocess.CalledProcessError:
+    print('yes, got exception')
