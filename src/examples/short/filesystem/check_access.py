2c3b16b4c82221330c1af5fc944feec35d882eea
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-10 23:37:20 +0300

    better example

--- src/examples/short/filesystem/check_access.py
+++ src/examples/short/filesystem/check_access.py
@@ -5,13 +5,17 @@ This is an example of how to check access for different files.
 
 NOTES:
 - if a file does not exists os.access will return 'False'
-on all types of access to it.
+on all types of access to it and not throw an exception.
+- this means that if os.access returns True it means that
+    1. the file exists.
+    2. the file is readable.
 """
 
 import os
 
-print(os.access('/etc/passwd', os.W_OK))
-print(os.access('/etc/passwd', os.R_OK))
-print(os.access('/tmp/doesnt_exist', os.R_OK))
-print(os.access('/tmp/doesnt_exist', os.W_OK))
-print(os.access('/tmp/doesnt_exist', os.X_OK))
+assert os.access('/etc/passwd', os.W_OK) == False
+assert os.access('/etc/passwd', os.R_OK) == True
+assert os.access('/etc/passwd', os.X_OK) == False
+assert os.access('/tmp/doesnt_exist', os.R_OK) == False
+assert os.access('/tmp/doesnt_exist', os.W_OK) == False
+assert os.access('/tmp/doesnt_exist', os.X_OK) == False
