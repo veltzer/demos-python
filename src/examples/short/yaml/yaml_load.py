8fc56adcbbacb0b586617f64388952043f481bff
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-20 06:18:20 +0300

    better example

--- src/examples/short/yaml/yaml_load.py
+++ src/examples/short/yaml/yaml_load.py
@@ -2,6 +2,8 @@
 
 """
 A basic example of loading YAML
+Make sure you use the "safe_load" method and not the "load" method
+that will give you warnings.
 
 References:
 - https://stackoverflow.com/questions/1773805/how-can-i-parse-a-yaml-file-in-python
@@ -11,6 +13,7 @@ import yaml
 
 with open("data_samples/basic.yaml", 'r') as stream:
     try:
-        print(yaml.load(stream))
+        data=yaml.safe_load(stream)
+        assert "concepts" in data
     except yaml.YAMLError as exc:
         print(exc)
