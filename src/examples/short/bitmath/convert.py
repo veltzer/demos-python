60f8c309c0199b7c871648ff419d75837bb44bf8
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-20 16:10:01 +0300

    added a bitmath example

---
+++ src/examples/short/bitmath/convert.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Show how to convert using bitmath
+
+References:
+- https://pypi.org/project/bitmath/
+"""
+
+import bitmath
+
+number = bitmath.Byte(100*1000*1000*1000)
+print(dir(number))
+print(number.to_GB())
