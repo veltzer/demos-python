4cac019fce4e38c3ad4fc65aeed52e0239c794ac
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-28 05:34:59 +0300

    found how to undo basicConfig of logging

---
+++ src/examples/short/logging/basic_config_undo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+This exmple explores how to undo what basicConfig does
+"""
+
+import logging
+import logging_tree
+
+
+logging_tree.printout()
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+logging_tree.printout()
+
+logger.warning("this is a warning message")
+logger.debug("this is a debug message")
+
+root_logger = logging.getLogger()
+for x in root_logger.handlers:
+    root_logger.removeHandler(x)
+
+logging_tree.printout()
+
+logger.warning("this is a warning message")
+logger.debug("this is a debug message")
