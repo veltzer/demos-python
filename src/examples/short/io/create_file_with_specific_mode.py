9c8199de52b1ee6c20f68871e129c3bad3ccd5aa
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-28 06:13:26 +0300

    another demo

---
+++ src/examples/short/io/create_file_with_specific_mode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+This example explores how to create a file with very specific mode bits.
+
+References:
+- https://stackoverflow.com/questions/5624359/write-file-with-specific-permissions-in-python/15015748
+"""
+
+import os
+
+prev_mask = os.umask(0o000)
+with os.fdopen(os.open("/tmp/test", os.O_WRONLY | os.O_CREAT, 0o644), 'wt') as f:
+    f.write("Hello, World!")
+os.umask(prev_mask)
