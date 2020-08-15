4eb220be8bed7918d247b8c8134d3e981e7afba6
Mark Veltzer <mark.veltzer@gmail.com>
2020-08-15 06:03:04 +0300

    more fixes

--- src/examples/short/logging/basic.py
+++ src/examples/short/logging/basic.py
@@ -5,15 +5,15 @@ NOTES:
 - The idea of passing __name__ to the logger
 is that you can turn logs on and off from certain modules.
 - the first time you create a logger it doesnt have any handlers
-and thats why you don't get any output despite the logging level
-begin set correctly. if you turn off do_addhandler below you will
+and that's why you don't get any output despite the logging level
+begin set correctly. if you turn off do_add_handler below you will
 see no logging at debug level.
 - to create another handler one must:
     create a handler
     set its level
     create a formatter
     set the formatting for the formatter
-    attach the fromatter to the handler
+    attach the formatter to the handler
     add the handler to the logger
 """
 
@@ -21,7 +21,7 @@ import logging
 
 import logging_tree
 
-do_addhandler = True
+do_add_handler = True
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -29,7 +29,7 @@ logger.setLevel(logging.DEBUG)
 logging_tree.printout()
 print(logger.handlers)
 
-if do_addhandler:
+if do_add_handler:
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(name)s %(levelname)s %(message)s')
