1bc52e75fd70cc289873edcbec1d7606515d6f70
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-11 04:19:54 +0300

    fix warnings

--- src/examples/short/ppt/change_ppt.py
+++ src/examples/short/ppt/change_ppt.py
@@ -4,10 +4,7 @@ Use the python-ppt to change a ppt
 References:
 - https://python-pptx.readthedocs.io/en/latest/#user-guide
 """
-from pptx import Presentation, shapes
-import sys
-
-from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pptx import Presentation
 
 input_filename = "data_samples/sample.pptx"
 output_filename = "/tmp/out.pptx"
@@ -23,13 +20,13 @@ for slide in slides:
     #     continue
     print(len(slide.shapes))
     # for shape in slide.shapes:
-        # print(type(shape.shape_type))
-        # print(dir(shape.shape_type))
+    # print(type(shape.shape_type))
+    # print(dir(shape.shape_type))
     #     if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
     #        print("deleting")
-            # shape.getparent().remove(shape)
+    # shape.remove(shape)
     #         slide.shapes.remove(shape)
-            # slide.remove(shape)
+    # slide.remove(shape)
     element = slide.background.element
     element.delete()
     # slide.background.element = None
--- src/examples/short/ppt/extract_text.py
+++ src/examples/short/ppt/extract_text.py
@@ -5,90 +5,11 @@ References:
 - https://www.syncfusion.com/kb/8672/how-to-extract-text-from-a-powerpoint-presentation
 - https://python-pptx.readthedocs.io/en/latest/#user-guide
 """
-import json
-import types
-from collections.abc import Iterable
+import sys
 from itertools import islice
-from pprint import pprint
 
 from pptx import Presentation
-import sys
-
 from pptx.enum.shapes import MSO_SHAPE_TYPE
-from var_dump import var_dump
-
-
-def dump(obj, level=0):
-    try:
-        for a in dir(obj):
-            # print(a)
-            if a.startswith("_"):
-                continue
-            if a in {"adjustments", "auto_shape_type"}:
-                continue
-            val = getattr(obj, a)
-            # print(type(val))
-            if type(val) in {int, float, str, list, dict, set}:
-                print(level*' ', val)
-            else:
-                dump(val, level=level+1)
-    except TypeError:
-        print(str(type(obj)))
-    except AttributeError:
-        print(str(type(obj)))
-
-
-def dump_obj(obj, level=0):
-    if hasattr(obj, '__dict__'):
-        for key, value in obj.__dict__.items():
-            if isinstance(value, (int, float, str, list, dict, set)):
-                print(" " * level + "%s -> %s" % (key, value))
-            else:
-                dump_obj(value, level + 2)
-
-
-def default_func(obj):
-    try:
-        return dir(obj)
-        # return obj.__dict__
-    except TypeError:
-        return str(type(obj))
-    except AttributeError:
-        return str(type(obj))
-
-
-def dump_json(obj):
-    # json.dump(obj, fp=sys.stdout, indent=2, default=default_func)
-    json.dump(obj, fp=sys.stdout, indent=2)
-
-
-def object_to_members_or_string(obj):
-    try:
-        if hasattr(obj, "__dict__"):
-            return vars(obj)
-        return str(obj)
-    except TypeError:
-        return str(obj)
-
-
-def flat_dump(obj) -> None:
-    """
-    Dump object to stdout. Only dumps the immediate members of the object
-    :param obj:
-    """
-    print("===========start dump=======")
-    try:
-        for x in dir(obj):
-            if x.startswith("_"):
-                continue
-            try:
-                v = getattr(obj , x)
-            except Exception:
-                v = "AttributeError"
-            print(f"{x} -> {v}")
-    except Exception:
-        print("Cannot flat_dump")
-
 
 input_filename = sys.argv[1]
 
