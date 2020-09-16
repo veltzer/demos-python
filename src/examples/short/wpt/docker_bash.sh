ebfad724ec2d84a040bdf22274be8d7c663e1ccd
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-16 04:11:03 +0300

    leave just essential stuff

--- src/examples/short/wpt/docker_bash.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# run bash on a container, given the image name of the containr
-
-image_name=$1
-id=$(docker ps --filter "ancestor=$image_name" --format "{{.ID}}")
-docker exec -it "$id" bash
