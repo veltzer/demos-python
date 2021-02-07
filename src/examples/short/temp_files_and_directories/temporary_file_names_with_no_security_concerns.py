f48b23fba95d014bca05b325cbc6cd43f0ebef28
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-07 06:54:26 +0200

    another note

--- src/examples/short/temp_files_and_directories/temporary_file_names_with_no_security_concerns.py
+++ src/examples/short/temp_files_and_directories/temporary_file_names_with_no_security_concerns.py
@@ -4,6 +4,8 @@ This is how to create temporary file names in python.
 This is not a secure method and subject to race conditions, but hell,
 sometimes, you don't care about security...
 
+'mktemp' is now deprecated. use 'mkstemp' instead.
+
 References:
 - https://docs.python.org/3/library/tempfile.html
 """
