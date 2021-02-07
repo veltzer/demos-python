b8cff715fad655586e321891960eae49cba4e7ef
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-07 05:08:58 +0200

    another example

---
+++ src/examples/short/requests/get_latest_release_from_github.py
@@ -0,0 +1,15 @@
+"""
+An example of getting the latest request of a package from github
+
+References:
+- https://stackoverflow.com/questions/60716016/how-to-get-the-latest-release-version-in-github-only-use-python-requests
+"""
+
+import requests
+
+owner='veltzer'
+repo='pyflexebs'
+
+response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/releases/latest")
+response.raise_for_status()
+print(response.json()["name"])
