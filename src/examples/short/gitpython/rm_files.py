8415ca127be4be16001a130292f300fa386be948
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-10 01:42:27 +0200

    yet another example

---
+++ src/examples/short/gitpython/rm_files.py
@@ -0,0 +1,10 @@
+"""
+How to remove files from git using git api
+
+References:
+- https://stackoverflow.com/questions/47041004/how-do-you-delete-a-file-with-gitpython
+"""
+
+import git
+
+repo = git.Repo(".")
