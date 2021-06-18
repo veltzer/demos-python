d7cff89eb4281903de2d49b73ead409df92b17d9
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-18 20:21:50 +0300

    another exercise

---
+++ src/exercises/basic/show_as_table/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+data=[]
+
+for line in sys.stdin:
+    line=line.rstrip()
+    data.append(line.split(" "))
+max_width=[0] * len(data[1])
+for datum in data:
+    i=0
+    for datoi in datum:
+        max_width[i]=max(max_width[i], len(datoi))
+        i+=1
+for datum in data:
+    i=0
+    for datoi in datum:
+        print(datoi + " " * (max_width[i]-len(datoi)+1), end="")
+        i+=1
+    print()
