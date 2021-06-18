0f027ae6a11321882ac22af3f4cfb858066957ab
Mark Veltzer <mark.veltzer@gmail.com>
2021-06-18 20:25:47 +0300

    make it easier

--- src/exercises/basic/show_as_table/solution.py
+++ src/exercises/basic/show_as_table/solution.py
@@ -7,6 +7,9 @@ data=[]
 for line in sys.stdin:
     line=line.rstrip()
     data.append(line.split(" "))
+# max_width = []
+# for k in data[1]:
+#     max_width.append(0)
 max_width=[0] * len(data[1])
 for datum in data:
     i=0
