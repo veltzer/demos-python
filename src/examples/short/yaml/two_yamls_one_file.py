b34b2ab4c7c0fb18787d3079f4631bcef06eb1a3
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-10 17:43:55 +0300

    added example of loading more than one document

---
+++ src/examples/short/yaml/two_yamls_one_file.py
@@ -0,0 +1,16 @@
+"""
+This example shows how to load two yamls from one file (using the "---" separator)
+
+References:
+- https://stackoverflow.com/questions/14359557/reading-yaml-file-with-python-results-in-yaml-composer-composererror-expected-a
+"""
+
+import yaml
+
+with open("data/yaml/two_documents_in_one_file.yaml", "r") as stream:
+    try:
+        documents = yaml.safe_load_all(stream)
+        for document in documents:
+            print(document)
+    except yaml.YAMLError as exc:
+        print(exc)
