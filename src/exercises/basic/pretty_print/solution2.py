fad78aa17021b8f6c8185b6280423620615ad92e
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-09 13:29:44 +0300

    linting

--- src/exercises/basic/pretty_print/solution2.py
+++ src/exercises/basic/pretty_print/solution2.py
@@ -1,9 +1,6 @@
-columns = [['H', 'He', 'Li', 'Be'], ['Hidrogen', 'Helium',
-                                     'Litium', 'Berilium'], ['1.008', '4.003', '6.941', '9.012']]
-
-
-# Before printing, we must know the width for each column.
-
+"""
+solution
+"""
 
 def max_len(lines):
     """Return the length of the longest string."""
@@ -13,25 +10,36 @@ def max_len(lines):
     return max(lengths)
 
 
-widths = []
-for column in columns:
-    widths.append(max_len(column))
+def main():
+    """ main function """
+    columns = [
+            ['H', 'He', 'Li', 'Be'],
+            ['Hidrogen', 'Helium', 'Litium', 'Berilium'],
+            ['1.008', '4.003', '6.941', '9.012'],
+    ]
+    # Before printing, we must know the width for each column.
+    widths = []
+    for column in columns:
+        widths.append(max_len(column))
 
-# The separator line always looks the same, so let's build it once:
-separator_line = '+'
-for w in widths:
-    separator_line += '-' * w + '+'
+    # The separator line always looks the same, so let's build it once:
+    separator_line = '+'
+    for width in widths:
+        separator_line += '-' * width + '+'
 
-# Now output separators alternating with text rows
+    # Now output separators alternating with text rows
 
-print(separator_line)
-
-# If you haven't seen this function(*args_list) syntax yet -
-# it's the same as zip(columns[0], columns[1], columns[2])
-# but would work for any number of columns.
-for row in zip(*columns):
-    line = '|'
-    for text, width in zip(row, widths):
-        line += text.ljust(width) + '|'
-    print(line)
     print(separator_line)
+
+    # If you haven't seen this function(*args_list) syntax yet -
+    # it's the same as zip(columns[0], columns[1], columns[2])
+    # but would work for any number of columns.
+    for row in zip(*columns):
+        line = '|'
+        for text, width in zip(row, widths):
+            line += text.ljust(width) + '|'
+        print(line)
+        print(separator_line)
+
+
+main()
