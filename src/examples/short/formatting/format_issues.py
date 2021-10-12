a1a65ba2985d381eb63185cbba1b8c89e544f37f
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-12 23:26:32 +0300

    more linting

--- src/examples/short/formatting/format_issues.py
+++ src/examples/short/formatting/format_issues.py
@@ -11,10 +11,12 @@ http://stackoverflow.com/questions/5466451/how-can-i-print-a-literal-characters-
 
 try:
     # noinspection PyStringFormat
+    # pylint: disable=missing-format-argument-key, consider-using-f-string
     s = '''{this will not work} {0}'''.format('mark')
     print(s)
-except Exception as e:
-    print('yes, got an exception', e)
+except KeyError as e:
+    print(f"yes got an exception {e}")
 
+# pylint: disable=consider-using-f-string
 s = '''{{this will work}} {0}'''.format('mark')
 print(s)
--- src/exercises/basic/sum_of_primes/solution.py
+++ src/exercises/basic/sum_of_primes/solution.py
@@ -2,10 +2,11 @@ import math
 
 
 def is_prime(n):
-    for j in range(2,int(math.sqrt(n))+1):
+    for j in range(2, int(math.sqrt(n)) + 1):
         if n % j == 0:
             return False
     return True
+
 
 my_sum = 0
 for i in range(2,1000):
