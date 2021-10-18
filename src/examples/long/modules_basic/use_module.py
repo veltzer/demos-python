4f341b3f19c5250398ec44a11ed66b67779cd90f
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-18 12:59:18 +0300

    more linting

--- src/examples/long/modules_basic/use_module.py
+++ src/examples/long/modules_basic/use_module.py
@@ -1,4 +1,4 @@
-# pylint: disable=relative-beyond-top-level
+# pylint: disable=relative-beyond-top-level,no-name-in-module
 from . import my_module
 print(f"hello from [{__file__}]")
 my_module.print_module_info()
