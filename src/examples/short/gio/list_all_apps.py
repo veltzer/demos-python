f39f2c9d670cdb07e047f9bcdcc9f4f0e67ff96e
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-24 08:18:50 +0300

    integrated another example

---
+++ src/examples/short/gio/list_all_apps.py
@@ -0,0 +1,9 @@
+from gi.repository import Gio
+
+all_apps = Gio.AppInfo.get_all()  # Returns a list of DesktopAppInfo objects (see docs)
+
+# For example, print display name and description of all apps
+for app in all_apps:
+    print(app.get_display_name())
+    print(f"\t{app.get_filename()}")
+    print(f"\t{app.get_description()}")
