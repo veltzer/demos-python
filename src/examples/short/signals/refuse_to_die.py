215111e8648b16738b4e571ff4939ad7071092e4
Mark Veltzer <mark.veltzer@gmail.com>
2021-11-14 23:24:35 +0200

    lint

--- src/examples/short/signals/refuse_to_die.py
+++ src/examples/short/signals/refuse_to_die.py
@@ -6,13 +6,14 @@ This program intercepts the SIGTERM signal and refuses to die...
 import signal
 import time
 
+
 def signal_handler(_sig, _frame):
     print('You send me SIGTERM, I Refuse to die!')
 
-signal.signal(signal.SIGTERM, signal_handler)
 
-i=0
+signal.signal(signal.SIGTERM, signal_handler)
+i = 0
 while True:
     print(f"[{i}] nag nag...")
-    i=i+1
+    i = i + 1
     time.sleep(1)
