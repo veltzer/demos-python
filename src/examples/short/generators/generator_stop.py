5e25463674f80d7660b587fd268a29a45ce01610
Mark Veltzer <mark.veltzer@gmail.com>
2022-02-15 14:09:42 +0200

    commit message

---
+++ src/examples/short/generators/generator_stop.py
@@ -0,0 +1,15 @@
+"""
+This example shows how to stop a generator using the return value
+of the generator function
+"""
+
+def generate():
+    for i in range(10):
+        if i==5:
+            print("yielding False")
+            yield False
+        print("yielding True")
+        yield True
+
+
+print(all(generate()))
