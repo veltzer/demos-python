aec53b7526fbf63c4206530141e155aa767f213a
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-14 13:25:14 +0300

    fix linting

--- src/examples/long/suppress_warnings_in_subprocess/parent.py
+++ src/examples/long/suppress_warnings_in_subprocess/parent.py
@@ -8,9 +8,9 @@ import os
 
 print("one")
 subprocess.check_call(["python3", "child.py"])
-os.environ["PYTHONWARNINGS"]="ignore"
+os.environ["PYTHONWARNINGS"] = "ignore"
 print("two")
 subprocess.check_call(["python3", "child.py"])
-os.environ["PYTHONWARNINGS"]="default"
+os.environ["PYTHONWARNINGS"] = "default"
 print("three")
 subprocess.check_call(["python3", "child.py"])
