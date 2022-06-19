6a5e0727cfffd5b7da1f3f1fbfcc8e460e9320d4
Mark Veltzer <mark.veltzer@gmail.com>
2022-06-19 05:41:39 +0300

    added to_integrate

---
+++ to_integrate/daemon/my_daemon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import logging.handlers
+import time
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+formatter = logging.Formatter('%(name)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+i=0
+while True:
+    logger.debug('this is debug mesage '+str(i))
+    i+=1
+    time.sleep(5)
