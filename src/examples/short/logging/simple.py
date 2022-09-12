28a1247f81c1bd7f30c28b92f6dd01f3509fdc99
Mark Veltzer <mark.veltzer@gmail.com>
2022-09-12 06:23:20 +0300

    no commit message given

--- src/examples/short/logging/simple.py
+++ src/examples/short/logging/simple.py
@@ -1,8 +1,24 @@
 """
 Most basic logging example
+
+It seems that here I do not have to call:
+        logging.basicConfig()
+but in other examples I did need to call it....
+
+Strange...
 """
 
 import logging
+
+
+def my_function():
+    my_logger = logging.getLogger(__name__)
+    my_logger.setLevel(logging.DEBUG)
+    my_logger.warning("this is a warning message %d", 1)
+    my_logger.debug("this is a debug message")
+    my_logger.info("this is an info message")
+    my_logger.error("this is an error message")
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -10,3 +26,4 @@ logger.warning("this is a warning message %d", 1)
 logger.debug("this is a debug message")
 logger.info("this is an info message")
 logger.error("this is an error message")
+my_function()
