ef02fa3fa822cdbb3cf58a4d954a28ddb0b01afd
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-31 09:03:02 +0200

    fix lint

--- src/examples/short/environment_variables/get_and_verify_exists.py
+++ src/examples/short/environment_variables/get_and_verify_exists.py
@@ -6,6 +6,10 @@ import os
 
 
 def get_strict(name: str):
+    """
+    this function gives you the value of an environment variable and fails
+    if it doesnt exist
+    """
     assert name in os.environ, f"missing environment variable [{name}]"
     return os.getenv(name)
 
