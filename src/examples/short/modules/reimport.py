a580ba81148333b030cd700601794f9cd77ab6f7
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-12 13:15:50 +0300

    linting

--- src/examples/short/modules/reimport.py
+++ src/examples/short/modules/reimport.py
@@ -7,7 +7,7 @@ import reimport_one
 
 print(f"add(2,2) is {reimport_one.add(2, 2)}")
 
-# pylint: disable=wrong-import-position, shadowed-import
+# pylint: disable=wrong-import-position
 import reimport_two as reimport_one  # noqa: E402
 
 print(f"add(2,2) is {reimport_one.add(2, 2)}")
--- src/examples/short/object_oriented/singleton/oo_singleton_1.py
+++ src/examples/short/object_oriented/singleton/oo_singleton_1.py
@@ -26,7 +26,6 @@ class A:
 
     def __init__(self):
         if A.instance is not None:
-            # pylint: disable=broad-exception-raised
             raise Exception('you called the constructor twice!!')
         # constructor code goes here
         print('in A constructor')
