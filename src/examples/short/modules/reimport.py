3dd9062be04a7016dd04bf675c91e311e5db06fe
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-13 18:19:58 +0300

    no commit message given

--- src/examples/short/modules/reimport.py
+++ src/examples/short/modules/reimport.py
@@ -7,7 +7,7 @@ import reimport_one
 
 print(f"add(2,2) is {reimport_one.add(2, 2)}")
 
-# pylint: disable=wrong-import-position
+# pylint: disable=shadowed-import, wrong-import-position
 import reimport_two as reimport_one  # noqa: E402
 
 print(f"add(2,2) is {reimport_one.add(2, 2)}")
