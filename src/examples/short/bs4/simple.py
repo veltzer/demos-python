25d654b64e290cc4dfdab4b19851e6e1234ac256
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-15 18:55:29 +0300

    first example of bs4

---
+++ src/examples/short/bs4/simple.py
@@ -0,0 +1,12 @@
+"""
+A simple demos for BeautifulSoup
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+url_link = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
+r = requests.get(url_link, timeout=5)
+r.raise_for_status()
+doc = BeautifulSoup(r.text, "html.parser")
+print(doc.prettify())
