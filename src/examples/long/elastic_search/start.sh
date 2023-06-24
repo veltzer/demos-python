6d0784e0b74795eccfea253290b5ee36a3bdad30
Mark Veltzer <mark.veltzer@gmail.com>
2023-06-25 00:15:01 +0300

    no commit message given

--- src/examples/long/elastic_search/start.sh
+++ src/examples/long/elastic_search/start.sh
@@ -7,6 +7,7 @@ NAME="elasticsearch:8.8.1"
 sudo sysctl -w vm.max_map_count=262144
 docker run\
 	-p 9200:9200\
+	--name elastic\
 	--detach\
 	-e ES_JAVA_OPTS="-Xms1g -Xmx1g"\
 	-e discovery.type=single-node\
--- src/examples/long/elastic_search/stop.sh
+++ src/examples/long/elastic_search/stop.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-NAME="elasticsearch:8.8.1"
-docker rm $(docker stop $(docker ps -a -q --filter ancestor="${NAME}" --format="{{.ID}}"))
+#!/bin/bash -e
+docker stop elastic
+docker rm elastic
