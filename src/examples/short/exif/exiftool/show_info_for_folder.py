6e482fec57093cad4ef70753724cf84fcbeec502
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-09 00:07:46 +0300

    first version of folder working tool

---
+++ src/examples/short/exif/exiftool/show_info_for_folder.py
@@ -0,0 +1,42 @@
+"""
+This script shows statistic are number of tags that exiftools finds per folder.
+It only scans '.mkv' files in that folder.
+"""
+
+import os
+import os.path
+import sys
+import subprocess
+
+
+def get_suffix(filename:str) -> str:
+    return os.path.splitext(filename)[1]
+
+
+def get_number_of_exiftool_tags(filename: str) -> int:
+    output = subprocess.check_output(["exiftool", filename], shell=False)
+    lines = output.decode("ascii").splitlines()
+    return len(lines)
+
+
+if len(sys.argv) != 2:
+    print(f"{sys.argv[0]}: usage: {sys.argv[0]} [FOLDER_NAME]")
+    sys.exit(1)
+
+folder_name = sys.argv[1]
+
+# recursively traverse all files in the folder
+for root, directories, files in os.walk(folder_name):
+    tag_numbers = []
+    for file in files:
+        full = os.path.join(root, file)
+        # if the file ends with ".mkv" add scan it with exiftool and add the number of tags
+        # seen to the statistics
+        suffix = get_suffix(full)
+        if suffix != ".mkv":
+            continue
+        print(f"scanning [{full}]")
+        tag_number = get_number_of_exiftool_tags(full)
+        tag_numbers.append(tag_number)
+print(f"min: {min(tag_numbers)}")
+print(f"max: {max(tag_numbers)}")
