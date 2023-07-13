701e2a7bbd035c8b4a877b7dfa5414a4bfae9e56
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-13 08:28:10 +0300

    added another example

---
+++ src/examples/short/urllib/remove_scheme.py
@@ -0,0 +1,17 @@
+"""
+This is an exmample of how to remove the scheme element from a url
+using the urllib python built-in module.
+
+References:
+- https://stackoverflow.com/questions/21687408/how-to-remove-scheme-from-url-in-python
+"""
+
+from urllib.parse import urlparse
+
+
+def strip_scheme(url: str) -> str:
+    schemaless = urlparse(url)._replace(scheme='').geturl()
+    return schemaless[2:]
+
+
+print(strip_scheme("https://google.com"))
