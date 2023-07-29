5748e58e275b2238e7fb62048464bdc0f74a1a3f
Mark Veltzer <mark.veltzer@gmail.com>
2023-07-29 18:07:37 +0300

    creating snippets with iobases

--- src/examples/short/make_video_snippet/create_with_iobase.py
+++ src/examples/short/make_video_snippet/create_with_iobase.py
@@ -7,12 +7,51 @@ The code was originally created by chatGPT
 
 
 import sys
+import io
+import os.path
+
 import cv2
+
+TEMP_FILE_IN = "/tmp/temp_in"
+# the file MUST be called *.mp4 because otherwise opencv will not be able to open it
+# as it will assume that it is a device or something.
+TEMP_FILE_OUT = "/tmp/temp_out.mp4"
+
+
+def write_stream_to_disk(stream, filename):
+    assert isinstance(stream, io.IOBase)
+    assert isinstance(filename, str)
+    with open(TEMP_FILE_IN, "wb") as out:
+        while b := stream.read(io.DEFAULT_BUFFER_SIZE):
+            out.write(b)
+
+
+def read_file_to_stream(filename, stream):
+    assert isinstance(filename, str)
+    assert isinstance(stream, io.IOBase)
+    with open(filename, "rb") as in_stream:
+        while b := in_stream.read(io.DEFAULT_BUFFER_SIZE):
+            stream.write(b)
+
+
+def create_video_snippet_streams(video_stream, snippet_stream, start_time, duration):
+    assert isinstance(video_stream, io.IOBase)
+    assert isinstance(snippet_stream, io.IOBase)
+    if os.path.isfile(TEMP_FILE_IN):
+        os.unlink(TEMP_FILE_IN)
+    if os.path.isfile(TEMP_FILE_OUT):
+        os.unlink(TEMP_FILE_OUT)
+    write_stream_to_disk(video_stream, TEMP_FILE_IN)
+    create_video_snippet(TEMP_FILE_IN, TEMP_FILE_OUT, start_time, duration)
+    assert os.path.isfile(TEMP_FILE_OUT)
+    read_file_to_stream(TEMP_FILE_OUT, snippet_stream)
+    os.unlink(TEMP_FILE_IN)
+    os.unlink(TEMP_FILE_OUT)
 
 
 def create_video_snippet(video_path, output_path, start_time, duration):
-    print(type(video_path))
-    print(type(output_path))
+    assert isinstance(video_path, str)
+    assert isinstance(output_path, str)
     cap = cv2.VideoCapture(video_path)
     cap.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)  # Set the start time in milliseconds
 
@@ -38,10 +77,14 @@ def create_video_snippet(video_path, output_path, start_time, duration):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"{sys.argv[0]}: usage: {sys.argv[0]} [VIDEO] [SNIPPET]")
-        sys.exit(1)
-    video = sys.argv[1]
-    snippet = sys.argv[2]
+    if len(sys.argv) == 3:
+        video = sys.argv[1]
+        snippet = sys.argv[2]
+    else:
+        video = "data/make_video_snippet/hitler_uses_git.mp4"
+        snippet = "data/make_video_snippet/hitler_uses_git_snippet.mp4"
+    if os.path.isfile(snippet):
+        os.unlink(snippet)
+    # create_video_snippet(video, snippet, 10, 5)
     with open(video, "rb") as s1, open(snippet, "wb") as s2:
-        create_video_snippet(s1, s2, 10, 5)
+        create_video_snippet_streams(s1, s2, 10, 5)
