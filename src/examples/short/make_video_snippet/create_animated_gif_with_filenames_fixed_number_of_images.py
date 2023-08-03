a6734112a9ca7add4c20e2cdc0492357fcd6a6f4
Mark Veltzer <mark.veltzer@gmail.com>
2023-08-03 22:47:19 +0300

    no commit message given

--- src/examples/short/make_video_snippet/create_animated_gif_with_filenames_fixed_number_of_images.py
+++ src/examples/short/make_video_snippet/create_animated_gif_with_filenames_fixed_number_of_images.py
@@ -15,12 +15,40 @@ import cv2
 import imageio
 
 
+def resize_image_keep_aspect(image, width, height):
+    """
+    This functin will receive an image, and will create a smaller version of it with dimentions:
+    width, height while *** keeping the orignal aspect ratio of the image ***.
+
+    https://stackoverflow.com/questions/43391205/add-padding-to-images-to-get-them-into-the-same-shape
+    """
+    h, w = image.shape[:2]
+    original_aspect: float = h / w
+    new_aspect: float = height / width
+    if original_aspect > new_aspect:
+        resize_factor = height / h
+        new_height = height
+        new_width = int(w * resize_factor)
+        tb_padding = 0
+        lr_padding = int((width - new_width) / 2)
+    else:
+        resize_factor = width / w
+        new_height = int(h * resize_factor)
+        new_width = width
+        tb_padding = int((height - new_height) / 2)
+        lr_padding = 0
+    resized_frame = cv2.resize(image, (new_width, new_height))
+    return cv2.copyMakeBorder(resized_frame, tb_padding, tb_padding, lr_padding, lr_padding, cv2.BORDER_CONSTANT)
+
+
 def create_animated_gif_from_video(
     video_path: str,
     output_path: str,
     number_of_images: int,
     duration_of_each_frame: int,  # in miliseconds
     loop: int,  # should the snippet loop
+    snippet_width: int,
+    snippet_height: int,
 ) -> None:
     cap = cv2.VideoCapture(video_path)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -35,7 +63,8 @@ def create_animated_gif_from_video(
         ret, frame = cap.read()
         assert ret is not None
         # we need this conversin to RGB for the animated gif to look right
-        resized_frame = cv2.resize(frame, (200,100))
+        # resized_frame = cv2.resize(frame, (snippet_width, snippet_height))
+        resized_frame = resize_image_keep_aspect(frame, snippet_width, snippet_height)
         rgb_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
         image_list.append(rgb_frame)
         pos += skip_frames
@@ -58,4 +87,6 @@ if __name__ == "__main__":
         number_of_images=10,
         duration_of_each_frame=1000,
         loop=0,
+        snippet_width=200,
+        snippet_height=100,
     )
