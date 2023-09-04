0110db99250e0a438219431f9f27f63300fee368
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-04 10:50:16 +0300

    no commit message given

--- src/exercises/basic/substring/not_fair.py
+++ src/exercises/basic/substring/not_fair.py
@@ -1,6 +1,7 @@
 def substring(s1, s2):
     return s1 in s2
 
+
 assert substring("hi", "Ship")
 assert not substring("shi", "Ship")
 assert not substring("Ship", "Shi")
--- src/exercises/basic/substring/not_fair_2.py
+++ src/exercises/basic/substring/not_fair_2.py
@@ -1,5 +1,6 @@
 def substring(s1, s2):
-    return s2.find(s1)!=-1
+    return s2.find(s1) != -1
+
 
 assert substring("hi", "Ship")
 assert not substring("shi", "Ship")
