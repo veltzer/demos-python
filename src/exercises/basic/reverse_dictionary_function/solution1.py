5c43b56bd26714b065f55d39e727e254c9d35537
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-04 16:28:00 +0300

    no commit message given

--- src/exercises/basic/reverse_dictionary_function/solution1.py
+++ src/exercises/basic/reverse_dictionary_function/solution1.py
@@ -1,19 +1,19 @@
 """
-this is a simple solution to the reverse hash function exercise
+this is a simple solution to the reverse dict function exercise
 """
 import doctest
 
 
-def rev_hash(o):
-    """ reverse a hash (build a value=>key mapping)
+def rev_dict(d):
+    """ reverse a dict (build a value=>key mapping)
 
-    >>> rev_hash({"Israel":"Jerusalem","France":"Paris","Italy":"Rome","Egypt":"Cairo"})
+    >>> rev_dict({"Israel":"Jerusalem","France":"Paris","Italy":"Rome","Egypt":"Cairo"})
     {'Jerusalem': 'Israel', 'Paris': 'France', 'Rome': 'Italy', 'Cairo': 'Egypt'}
     """
-    ret = {}
-    for k, v in o.items():
-        ret[v] = k
-    return ret
+    rev_d = {}
+    for k, v in d.items():
+        rev_d[v] = k
+    return rev_d
 
 
 doctest.testmod()
