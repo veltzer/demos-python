c5f796d9d80608c3d97e3f911721d428e5ac5540
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-11 14:47:51 +0300

    no commit message given

---
+++ src/exercises/basic/oo/solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+class Person:
+    def __init__(self, id, name, surname):
+        self.id=id
+        self.name=name
+        self.surname=surname
+    def __repr__(self):
+        return f"<id {self.id}, name {self.name}, surname {self.surname}>"
+    def __str__(self):
+        return f"<id {self.id}, name {self.name}, surname {self.surname}>"
+
+people = []
+id_to_person = {}
+with open("people.csv", "rt") as stream:
+    next(stream) # skip the first line
+    for line in stream:
+        line=line.rstrip() # remove the newline at the end
+        values = line.split(",")
+        p = Person(*values)
+        id_to_person[p.id]=p
+        people.append(p)
+
+print(people)
