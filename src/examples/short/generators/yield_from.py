d90001d8343dded1b0c92931188deb0d1deb2178
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-12 16:13:38 +0300

    no commit message given

--- src/examples/short/generators/yield_from.py
+++ src/examples/short/generators/yield_from.py
@@ -1,15 +1,17 @@
-
 def yield_some():
     yield 1
     yield 2
+
 
 def yield_more():
     yield 3
     yield 4
 
+
 def yield_both():
     yield from yield_some()
     yield from yield_more()
+
 
 for x in yield_both():
     print(x)
