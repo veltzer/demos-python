58d5e21d706c9f694f57cceb5565eb50e9644b7e
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-19 18:40:32 +0300

    no commit message given

---
+++ src/exercises/basic/args_concatenate_lists/solution.py
@@ -0,0 +1,25 @@
+
+
+def catenate_lists(*args):
+    r = []
+    for e in args:
+        r.extend(e)
+    return r
+
+def catenate_lists_2(*args):
+    r = []
+    for e in args:
+        for x in e:
+            r.append(x)
+    return r
+
+
+l1=[1,2,3]
+l2=[4,5,6,7]
+l3=[8,9,10]
+
+r1 = catenate_lists(l1, l2, l3)
+r2 = catenate_lists(l1, l3)
+
+print(r1)
+print(r2)
