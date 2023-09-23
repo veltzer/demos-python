176ca6c0f1da71faf2c3810492f84f7ec127700b
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-23 10:32:09 +0300

    no commit message given

---
+++ src/examples/short/colorama/demo.py
@@ -0,0 +1,8 @@
+"""
+This is a demo for the "colorama" module
+"""
+
+from colorama import Back, Style
+
+print(Back.GREEN + 'The text with Green background')
+print(Style.DIM + 'The text is DIM now')
