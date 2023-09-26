b11d4a1c770d3e96d9097a9c72a26ff2ded2374b
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-26 19:53:16 +0300

    solution to closure exercise

---
+++ src/exercises/advanced/oo_using_closure_simple/solution.py
@@ -0,0 +1,30 @@
+def make_a_stack():
+    my_list = []
+
+    def push(x):
+        my_list.append(x)
+
+    def pop():
+        return my_list.pop()
+
+    def show():
+        print(my_list)
+
+    return (push, pop, show)
+
+
+def main():
+    push_function, pop_function, show_function = make_a_stack()
+    show_function()
+    push_function(5)
+    show_function()
+    push_function(7)
+    show_function()
+    _ = pop_function()
+    show_function()
+    _ = pop_function()
+    show_function()
+
+
+if __name__ == "__main__":
+    main()
