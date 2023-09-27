8816f3f92ad4a0a31092fe8d883e39451fc8468c
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-27 21:39:37 +0300

    no commit message given

---
+++ src/examples/short/language/decorators/pass_parameter.py
@@ -0,0 +1,15 @@
+"""
+This is an example of how to pass parameters to a decorator. 
+"""
+
+def multply_by(func, n):
+    def inner(*arg, **kwargs):
+        return func(*arg, **kwargs) * n
+    return inner
+
+def add(a, b):
+    return a+b
+
+add = multply_by(add, 5)
+
+print(f"did you know that 2+2={add(2,2)}")
---
+++ src/examples/short/language/decorators/pass_parameter_better.py
@@ -0,0 +1,18 @@
+"""
+This is an example of how to pass parameters to a decorator. 
+"""
+
+def multply_by(n):
+    def decorator_func(func):
+        def inner(*arg, **kwargs):
+            return func(*arg, **kwargs) * n
+        return inner
+    return decorator_func
+
+@multply_by(n=5)
+def add(a, b):
+    return a+b
+
+# add = multply_by(5)
+
+print(f"did you know that 2+2={add(2,2)}")
