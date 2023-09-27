9338b2738de5df283a17603408efbcb0cfbd3794
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-27 19:09:34 +0300

    no commit message given

---
+++ src/exercises/advanced/cache_decorator/solution.py
@@ -0,0 +1,12 @@
+import math
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)+1)):
+        if n%i==0:
+            return False
+    return True
+
+# test 'is_prime'
+# for i in range(100):
+#     if is_prime(i):
+#         print(i)
