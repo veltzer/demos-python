74365104b547681196465d411f122ce8e04494d0
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-28 17:56:26 +0300

    no commit message given

--- src/examples/short/modules/logging/syslog.py
+++ src/examples/short/modules/logging/syslog.py
@@ -8,6 +8,7 @@ and who the message came from
 """
 
 import os
+import time
 import logging.handlers
 
 logger = logging.getLogger(__name__)
@@ -17,7 +18,12 @@ formatter = logging.Formatter("%(name)s %(levelname)s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-logger.debug("this is debug")
-logger.critical("this is critical")
+print("please run 'tail -f /var/log/syslog' to see the logs...")
+counter = 0
+while True:
+    logger.debug(f"this is debug {counter}")
+    logger.critical(f"this is critical {counter}")
+    counter += 1
+    time.sleep(1)
 
-os.system("tail /var/log/syslog")
+# os.system("tail /var/log/syslog")
