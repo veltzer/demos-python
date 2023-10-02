538e1807aebb037cf395755051a89f67682fff40
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-02 12:50:18 +0300

    no commit message given

--- src/examples/short/language/exec/get_back_value.py
+++ src/examples/short/language/exec/get_back_value.py
@@ -3,7 +3,7 @@ This is an example of how to get a value back from a piece
 of code you execute with the "exec" function.
 """
 
-d = {}
+d = {}  # type: ignore
 # pylint: disable=exec-used
 exec("ret=7", {}, d)
 print(d["ret"])
