a9db4924e22c021f93eaea31e3f6c838f66718c4
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-31 18:39:27 +0200

    no commit message given

--- src/examples/short/language/memory/really_simple.py
+++ src/examples/short/language/memory/really_simple.py
@@ -1,5 +1,5 @@
 from sys import getsizeof
 
-l = [0,1,2,3,4,5,6,7,8,9, [0]*1000000]
+long_list = [0,1,2,3,4,5,6,7,8,9, [0] * 1000000]
 
-print(f"l takes up {getsizeof(l)} bytes")
+print(f"long_list takes up {getsizeof(long_list)} bytes")
