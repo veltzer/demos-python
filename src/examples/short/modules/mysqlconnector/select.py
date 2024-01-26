37b7e22e32299d9ca2c06531c0671a6aedb16fae
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-27 00:53:58 +0200

    no commit message given

---
+++ src/examples/short/modules/mysqlconnector/select.py
@@ -0,0 +1,29 @@
+"""
+This is an example showing how to use the mysql.connector
+module to select records from a mysql database.
+
+This module comes from the mysql-connection.
+"""
+
+import os.path
+import mysql.connector
+
+
+def main():
+    option_files = os.path.expanduser("~/.my.cnf")
+    option_groups = "clientmyworld"
+    db = mysql.connector.connect(
+        option_files=option_files,
+        option_groups=option_groups,
+        database="mysql",
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT user FROM user WHERE host=%(host)s", {"host": "localhost"})
+    for row in cursor:
+        for datum in row:
+            print(datum.decode())
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/examples/short/modules/mysqlconnector/show_databases.py
@@ -0,0 +1,28 @@
+"""
+This is an example showing how to use the mysql.connector
+module to connect and interact with the mysql database.
+
+This module comes from the mysql-connection.
+"""
+
+import os.path
+import mysql.connector
+
+
+def main():
+    option_files = os.path.expanduser("~/.my.cnf")
+    option_groups = "clientmyworld"
+    db = mysql.connector.connect(
+        option_files=option_files,
+        option_groups=option_groups,
+    )
+    cursor = db.cursor()
+    cursor.execute("SHOW DATABASES")
+    for row in cursor:
+        for datum in row:
+            print(datum.decode())
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
