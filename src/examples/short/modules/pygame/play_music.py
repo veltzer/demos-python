80c65a7571131aca652431a4b37bdac905e548ac
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-08 18:08:36 +0200

    no commit message given

--- src/examples/short/modules/pygame/play_music.py
+++ src/examples/short/modules/pygame/play_music.py
@@ -1,25 +1,31 @@
-import pygame
 import os
+import pygame
 
-# Initialize pygame mixer
-pygame.mixer.init()
 
-# Load rendered piano samples
-sample_dir = "rendered_samples"
-samples = {}
-for filename in os.listdir(sample_dir):
-    if filename.endswith(".wav"):
-        note_name = filename.split(".")[0]
-        samples[note_name] = pygame.mixer.Sound(os.path.join(sample_dir, filename))
-
-# Play a note
-def play_note(note_name):
+def play_note(samples, note_name):
+    """ play a note """
     if note_name in samples:
         samples[note_name].play()
 
-# Example usage
-play_note("C4")
-pygame.time.delay(2000)
 
-# Clean up
-pygame.mixer.quit()
+def main():
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Load rendered piano samples
+    sample_dir = "rendered_samples"
+    samples = {}
+    for filename in os.listdir(sample_dir):
+        if filename.endswith(".wav"):
+            note_name = filename.split(".")[0]
+            samples[note_name] = pygame.mixer.Sound(os.path.join(sample_dir, filename))
+    # Example usage
+    play_note(samples, "C4")
+    pygame.time.delay(2000)
+
+    # Clean up
+    pygame.mixer.quit()
+
+
+if __name__ == "__main__":
+    main()
--- src/examples/short/modules/simpleaudio/play_wav.py
+++ src/examples/short/modules/simpleaudio/play_wav.py
@@ -1,8 +1,8 @@
-import simpleaudio
-
 """
 Example of how to play a .wav file using simpleaudio
 """
+
+import simpleaudio
 
 wave_obj = simpleaudio.WaveObject.from_wave_file("data_samples/wav/sample.wav")
 play_obj = wave_obj.play()
