5e884186ef44a5595f1a8a1ce7825c10e9fa9425
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-25 16:25:42 +0300

    elaborated the example

--- src/examples/short/language/functions/mutable_default_values.py
+++ src/examples/short/language/functions/mutable_default_values.py
@@ -13,6 +13,19 @@ def add_name(name: str, target: list[str] = []) -> list[str]:
     return target
 
 
+def add_name_fixed(name: str, target: None | list[str] = None) -> list[str]:
+    if target is None:
+        target = []
+    target.append(name)
+    return target
+
+
 print(add_name("Alice"))
 print(add_name("Bob"))
+print(add_name("Barbie", target=[]))
 print(add_name("Charlie"))
+print("now for the fixed version")
+print(add_name_fixed("Alice"))
+print(add_name_fixed("Bob"))
+print(add_name_fixed("Barbie", target=[]))
+print(add_name_fixed("Charlie"))
