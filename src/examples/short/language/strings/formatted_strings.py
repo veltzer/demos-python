a613eb64fe0dfa89724fd271b8f5f5f654a7c1b2
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-26 03:15:08 +0300

    no commit message given

---
+++ src/examples/short/language/strings/formatted_strings.py
@@ -0,0 +1,19 @@
+"""
+This example shows how to use formatted strings in python.
+"""
+
+name = "Charlie"
+age = 35
+
+print(f"My name is {name} and I'm {age} years old.")
+# Output: My name is Charlie and I'm 35 years old.
+
+pi = 3.14159
+print(f"The value of pi is approximately {pi:.2f}")
+# Output: The value of pi is approximately 3.14
+
+# This prints both the expression and it's value, very useful
+a = 5
+b = 6
+print(f"{a+b=}")
+# Output: a+b=11
