ee2225c98701513903e4846b0c7edfbacc8cb036
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-27 10:38:22 +0300

    no commit message given

---
+++ src/examples/long/flask_static/README.txt
@@ -0,0 +1,5 @@
+This example explores flask and the static folder
+
+Conclusions:
+- Flask does not automatically serve "index.html" if no route is given.
+- Flask's default static folder is "static".
---
+++ src/examples/long/flask_static/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+A simple web server
+"""
+
+
+import flask
+
+
+app = flask.Flask("app")
+app.config["DEBUG"] = True
+# static_folder = app.config["STATIC_FOLDER"]
+print(f"{app.static_folder=}")
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="0.0.0.0")
