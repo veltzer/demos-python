968498d4c4425c9182b1dfd9284a96403f6d239b
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-17 06:25:31 +0300

    no commit message given

--- src/examples/short/modules/selenium/open_gmail.py
+++ src/examples/short/modules/selenium/open_gmail.py
@@ -1,11 +1,12 @@
 """
 This is an example of how to open gmail (real one with session) using selenium.
 
-Turns out this is a really hard thing to do since google really don"t want someone
+Turns out this is a really hard thing to do since google really dont want someone
 to open gmail in selenium.
 
-I tried with both firefox and chrom (see below) but to no avail.
+I tried with both firefox and chrome (see below) but to no avail.
 """
+
 import os.path
 import time
 
@@ -18,15 +19,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-browser = "chrome"
+browser = "firefox"
 if browser == "chrome":
     options = selenium.webdriver.chrome.options.Options()
     options.add_argument("--headless")  # do not really open a window
     options.add_argument("--disable-gpu")  # Last I checked this was necessary.
-    user_data_dir = os.path.expanduser("~/.config/google-chrome/Default")
     user_data_dir = os.path.expanduser("~/.config/google-chrome")
     assert os.path.isdir(user_data_dir)
-    print(user_data_dir)
     options.add_argument(f"--user-data-dir={user_data_dir}")
     chrome_driver = Chrome(
         service=selenium.webdriver.chrome.service.Service(ChromeDriverManager().install()),
