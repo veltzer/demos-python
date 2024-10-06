4fb1c11d717b4cc1f2e273b870739afe1de11a5c
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-06 17:50:43 +0300

    reorg

--- src/to_integrate/modules/keyring/get.py
+++
@@ -1,8 +0,0 @@
-"""
-A basic example of how to get a password from the default
-keyring of you environment.
-"""
-
-import keyring
-password = keyring.get_password("dummyapp", "mark.veltzer@gmail.com")
-print(f"your password is [{password}]")
--- src/to_integrate/modules/keyring/set.py
+++
@@ -1,6 +0,0 @@
-"""
-This is an example of setting a password.
-"""
-
-import keyring
-keyring.set_password("dummyapp", "mark.veltzer@gmail.com", "thisisthepassword")
