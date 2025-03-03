242426ce1bb0ee5a1e07cbfbc6ecff31e83dc677
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-03 02:34:28 +0200

    added another exercise

---
+++ src/exercises/basic/map_reduce/exercise.md
@@ -0,0 +1,7 @@
+# Map-Reduce exercise
+
+Calculate the sum of 1*1 +  2*2 + 3*3 + ... 100*100
+
+in two ways:
+* regular imperative way
+* functional way using `map` and `functools.reduce`
---
+++ src/exercises/basic/map_reduce/solution.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+
+# imperative coding
+mysum = 0
+for x in range(100):
+    mysum = mysum + x * x
+print(f"mysum is {mysum}")
+
+# functional , declarative coding
+m = map(lambda x: x * x, range(100))
+result = reduce(lambda a,b: a + b, m)
+print(f"result is {result}")
